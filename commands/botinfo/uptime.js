ce568bca16605f361fb2d4aaa966679602b9b6c8
y3ll0wlife <y3ll0wlife@protonmail.com>
2020-09-15 21:44:01 +0200

    New command: uptime

---
+++ commands/botinfo/uptime.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js");
+const moment = require("moment");
+require("moment-duration-format");
+
+module.exports = {
+	name: "uptime",
+	aliases: ["status"],
+	category: "Info",
+	description: "Get the current uptime of the bot",
+	run: async (client, message, args) => {
+		let duration = moment.duration(client.uptime).format(" D [days], H [hrs], m [mins], s [secs]");
+
+		let embed = new Discord.MessageEmbed()
+			.setColor(process.env.COLOR)
+			.setTimestamp()
+			.setFooter(`Executed by ${message.author.username}#${message.author.discriminator}`, message.author.avatarURL())
+			.setAuthor(`${client.user.username}#${client.user.discriminator}`, client.user.avatarURL())
+			.setTitle(`Current uptime: ${duration}`);
+
+		return message.channel.send(embed);
+	},
+};
