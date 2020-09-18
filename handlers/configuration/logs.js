7149b218ec96c5f06fe183cf493fcf6ba95bac83
y3ll0wlife <y3ll0wlife@protonmail.com>
2020-09-18 17:07:29 +0200

    Update logs.js

--- handlers/configuration/logs.js
+++ handlers/configuration/logs.js
@@ -1,3 +1,46 @@
 const Log = require("../../models/log");
 
-module.exports = {};
+module.exports = {
+	Change_Mod_Action: async (guildId, channelId) => {
+		Log.findOneAndUpdate({ guildID: guildId }, { modAction: channelId }, function (error) {
+			if (error) {
+				console.error(clc.red(error));
+			}
+		});
+	},
+	Change_Message: async (guildId, channelId) => {
+		Log.findOneAndUpdate({ guildID: guildId }, { message: channelId }, function (error) {
+			if (error) {
+				console.error(clc.red(error));
+			}
+		});
+	},
+	Change_Role: async (guildId, channelId) => {
+		Log.findOneAndUpdate({ guildID: guildId }, { role: channelId }, function (error) {
+			if (error) {
+				console.error(clc.red(error));
+			}
+		});
+	},
+	Change_Member: async (guildId, channelId) => {
+		Log.findOneAndUpdate({ guildID: guildId }, { member: channelId }, function (error) {
+			if (error) {
+				console.error(clc.red(error));
+			}
+		});
+	},
+	Change_Channel: async (guildId, channelId) => {
+		Log.findOneAndUpdate({ guildID: guildId }, { channel: channelId }, function (error) {
+			if (error) {
+				console.error(clc.red(error));
+			}
+		});
+	},
+	Change_Join_Leave: async (guildId, channelId) => {
+		Log.findOneAndUpdate({ guildID: guildId }, { join_leave: channelId }, function (error) {
+			if (error) {
+				console.error(clc.red(error));
+			}
+		});
+	},
+};
