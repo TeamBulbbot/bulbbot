7b77685c6a2a9ce4279dcd11563c24bcbc7382bb
y3ll0wlife <y3ll0wlife@protonmail.com>
2020-09-20 12:00:49 +0200

    Update about.js

--- commands/botinfo/about.js
+++ commands/botinfo/about.js
@@ -19,7 +19,7 @@ module.exports = {
 **🛠️ Developers**
 **Ben!**#0002 \`\`(255834596766253057)\`\`
 **KlukCZ**#6589 \`\`(439396770695479297)\`\`
-**mrphilip**#0882 \`\`(190160914765316096)\`\`
+**mrphilip**#0001 \`\`(190160914765316096)\`\`
 
 **📜 License**
 This bot is licensed under the MIT license, for more info please see the full license **[here](https://github.com/TestersQTs/Bulbbot/blob/master/LICENSE)**
@@ -27,7 +27,7 @@ This bot is licensed under the MIT license, for more info please see the full li
 			)
 			.addField("Github", "**[Link](https://github.com/TestersQTs/Bulbbot)**", true)
 			.addField("Invite bot", "**Invite link goes here**", true)
-			.addField("Privacy Policy", "**[Link](https://github.com/TestersQTs/Bulbbot/blob/master//Privacy%20Policy.md)**", true);
+			.addField("Privacy Policy", "**[Link](https://www.notion.so/Bulbbot-Privacy-Policy-23188b59422e4f089a0379ae34715cd7#c43815d9fb56427380c7c639fb77b3ea)**", true);
 
 		return message.channel.send(embed);
 	},
