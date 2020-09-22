ff43866dabfe269851a7461fac93165a710b416c
y3ll0wlife <y3ll0wlife@protonmail.com>
2020-09-22 21:05:10 +0200

    New events

---
+++ events/guildBanRemove.js
@@ -0,0 +1,5 @@
+const SendLog = require("../handlers/SendLog");
+
+module.exports = async (client, guild, user) => {
+	//SendLog.Mod_action(client, guild.id, `Unbanned **${user.username}**#${user.discriminator} \`\`(${user.id})\`\` by **Manual Unban**`, "");
+};
