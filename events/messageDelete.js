e4645f9a1b735c2c5c0b19002c48090492e9bd1a
y3ll0wlife <y3ll0wlife@protonmail.com>
2020-09-22 21:05:26 +0200

    Moved to another file

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -1,19 +1,8 @@
-const Log = require("../models/log");
-const moment = require("moment");
+const SendLog = require("../handlers/SendLog");
 const Emotes = require("../emotes.json");
 
 module.exports = async (client, message) => {
 	if (message.author.bot) return;
-	Log.findOne(
-		{
-			guildID: message.guild.id,
-		},
-		async (err, res) => {
-			if (err) console.error(`[Message Delete] ${clc.red(err)}`);
-			if (res.message === "") return;
-			client.channels.cache
-				.get(res.message)
-				.send(`\`\`[${moment().format("hh:mm:ss a")}]\`\` ${Emotes.other.trash} Message from **${message.author.username}**#${message.author.discriminator} was deleted in **${message.channel.name}** \`\`(${message.channel.id})\`\`\n**Content:** ${message.content}`);
-		}
-	);
+
+	SendLog.Message_Log(client, message.guild.id, `${Emotes.other.trash} Message from **${message.author.username}**#${message.author.discriminator} was deleted in **${message.channel.name}** \`\`(${message.channel.id})\`\`\n**Content:** ${message.content}`);
 };
--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -1,25 +1,13 @@
-const Log = require("../models/log");
-const moment = require("moment");
+const SendLog = require("../handlers/SendLog");
 const Emotes = require("../emotes.json");
-var clc = require("cli-color");
 
 module.exports = async (client, oldMessage, newMessage) => {
 	if (oldMessage.author.bot) return;
 	if (oldMessage.content === newMessage.content) return; // Message was not edit but something else happend ( ͡° ͜ʖ ͡°)
-	Log.findOne(
-		{
-			guildID: newMessage.guild.id,
-		},
-		async (err, res) => {
-			if (err) console.error(`[Message Update] ${clc.red(err)}`);
-			if (res.message === "") return;
-			client.channels.cache
-				.get(res.message)
-				.send(
-					`\`\`[${moment().format("hh:mm:ss a")}]\`\` ${Emotes.other.wrench} Message from **${newMessage.author.username}**#${newMessage.author.discriminator} was edited in **${newMessage.channel.name}** \`\`(${newMessage.channel.id})\`\`\n**Old Message:** ${oldMessage.content}\n**New Message:** ${
-						newMessage.content
-					}`
-				);
-		}
+
+	SendLog.Message_Log(
+		client,
+		newMessage.guild.id,
+		`${Emotes.other.wrench} Message from **${newMessage.author.username}**#${newMessage.author.discriminator} was edited in **${newMessage.channel.name}** \`\`(${newMessage.channel.id})\`\`\n**Old Message:** ${oldMessage.content}\n**New Message:** ${newMessage.content}`
 	);
 };
