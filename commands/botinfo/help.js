efcca188fb655e5baea5583e942880cffed331b5
y3ll0wlife <y3ll0wlife@protonmail.com>
2020-09-23 15:42:35 +0200

    Update help.js

--- commands/botinfo/help.js
+++ commands/botinfo/help.js
@@ -9,7 +9,14 @@ module.exports = {
 			.setColor(process.env.COLOR)
 			.setThumbnail(client.user.avatarURL())
 			.setFooter(`Executed by ${message.author.username}#${message.author.discriminator}`, message.author.avatarURL())
-			.setAuthor(`Help ${client.user.username}#${client.user.discriminator}`, client.user.avatarURL());
+			.setAuthor(`Help ${client.user.username}#${client.user.discriminator}`, client.user.avatarURL()).setDescription(`
+			**Usage of commands**
+			\`\`prefix\`\` (default \`\`!\`\`) \`\`commandname\`\` \`\`arguments\`\`
+			| = Aliases
+			<> = Arugment is required
+			[] = Argument is optional
+			
+			`);
 		client.commands.forEach((c) => {
 			if (c.category !== "developer") {
 				embed.addField(`${c.name} \`\`(${c.category})\`\``, ` ${c.aliases > 0 ? `> **Aliases:** ${c.aliases.join("**|**")}` : c.aliases === undefined ? "" : `> **Aliases:** ${c.aliases.join("**|**")}`}\n> **Description**: ${c.description}`);
