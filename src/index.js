bcfec1891b4512b5c3ae8b3d6bbcf05f31810897
ent3r <32072697+ent3r@users.noreply.github.com>
2020-09-24 08:48:22 +0200

    :construction: Add `resolve` for folder resolution

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const { config } = require("dotenv");
 const fs = require("fs");
 const mongoose = require("mongoose");
 const ascii = require("ascii-table");
+const { resolve } = require("path");
 const table = new ascii().setHeading("Event", "Status");
 
 const client = new Client();
@@ -11,27 +12,27 @@ client.commands = new Collection();
 client.aliases = new Collection();
 client.mongoose = require("./utils/mongoose");
 
-client.categories = fs.readdirSync("./commands/");
+client.categories = fs.readdirSync(resolve(__dirname, "./commands/"));
 
 config({
-	path: `${__dirname}/.env`,
+  path: `${__dirname}/.env`,
 });
 
 ["command"].forEach((handler) => {
-	require(`./handlers/${handler}`)(client);
+  require(`./handlers/${handler}`)(client);
 });
 
-fs.readdir("./events/", (err, files) => {
-	if (err) return console.error;
-	files.forEach((file) => {
-		if (!file.endsWith(".js")) return;
-		const evt = require(`./events/${file}`);
-		let evtName = file.split(".")[0];
+fs.readdir(resolve(__dirname, "./events/"), (err, files) => {
+  if (err) return console.error;
+  files.forEach((file) => {
+    if (!file.endsWith(".js")) return;
+    const evt = require(`./events/${file}`);
+    let evtName = file.split(".")[0];
 
-		table.addRow(evtName, "✅");
-		client.on(evtName, evt.bind(null, client));
-	});
-	console.log(table.toString());
+    table.addRow(evtName, "✅");
+    client.on(evtName, evt.bind(null, client));
+  });
+  console.log(table.toString());
 });
 
 client.mongoose.init();
