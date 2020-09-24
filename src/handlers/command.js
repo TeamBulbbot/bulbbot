f0465b555e396a330bd66347f491de8154d18ad1
ent3r <32072697+ent3r@users.noreply.github.com>
2020-09-24 11:29:37 +0200

    Fix folder resolution

--- src/handlers/command.js
+++ src/handlers/command.js
@@ -1,25 +1,29 @@
 const { readdirSync } = require("fs");
 const ascii = require("ascii-table");
+const { resolve } = require("path");
 const table = new ascii("Discord Bot").setHeading("Command", "Status");
 
 module.exports = (client) => {
-	readdirSync("./commands/").forEach((dir) => {
-		const commands = readdirSync(`./commands/${dir}/`).filter((f) => f.endsWith(".js"));
+  readdirSync(resolve(__dirname, "../commands/")).forEach((dir) => {
+    const commands = readdirSync(
+      resolve(__dirname, `../commands/${dir}/`)
+    ).filter((f) => f.endsWith(".js"));
 
-		for (let file of commands) {
-			let pull = require(`../commands/${dir}/${file}`);
+    for (let file of commands) {
+      let pull = require(`../commands/${dir}/${file}`);
 
-			if (pull.name) {
-				client.commands.set(pull.name, pull);
-				table.addRow(file, "✅");
-			} else {
-				table.addRow(file, "❌");
-				continue;
-			}
+      if (pull.name) {
+        client.commands.set(pull.name, pull);
+        table.addRow(file, "✅");
+      } else {
+        table.addRow(file, "❌");
+        continue;
+      }
 
-			if (pull.aliases && Array.isArray(pull.aliases)) pull.aliases.forEach((alias) => client.aliases.set(alias, pull.name));
-		}
+      if (pull.aliases && Array.isArray(pull.aliases))
+        pull.aliases.forEach((alias) => client.aliases.set(alias, pull.name));
+    }
 
-		console.log(table.toString());
-	});
+    console.log(table.toString());
+  });
 };
