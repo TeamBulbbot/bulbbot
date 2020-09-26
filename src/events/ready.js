cf2893cf45ccabddc0730127691a3d184cbf39bc
y3ll0wlife <y3ll0wlife@protonmail.com>
2020-09-26 16:47:38 +0200

    Update ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,13 +1,21 @@
 var clc = require("cli-color");
 
 module.exports = (client) => {
-  console.log(clc.cyan("[!] Bot is online"));
+	console.log(
+		clc.cyan(`
+[!] Username: ${client.user.username}
+[!] ID: ${client.user.id}
+[!] Guild Count: ${client.guilds.cache.size}
+[!] Commands: ${client.commands.size}
+[!] Bot is online
+  `)
+	);
 
-  client.user.setPresence({
-    status: "online",
-    activity: {
-      name: `over ${client.guilds.cache.size} guilds`,
-      type: "WATCHING",
-    },
-  });
+	client.user.setPresence({
+		status: "online",
+		activity: {
+			name: `over ${client.guilds.cache.size} guilds`,
+			type: "WATCHING",
+		},
+	});
 };
