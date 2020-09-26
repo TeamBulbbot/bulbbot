db1b86a12003ae44f3089ddccb2d0ad810fa55b4
y3ll0wlife <y3ll0wlife@protonmail.com>
2020-09-26 16:47:35 +0200

    Changed from var to const

--- src/events/guildCreate.js
+++ src/events/guildCreate.js
@@ -1,7 +1,7 @@
 const Database = require("../handlers/Database");
-var clc = require("cli-color");
+const clc = require("cli-color");
 
 module.exports = async (client, guild) => {
-  Database.AddGuild(guild);
-  console.log(clc.green("[+] Joined new server"));
+	Database.AddGuild(guild);
+	console.log(clc.green("[+] Joined new server"));
 };
