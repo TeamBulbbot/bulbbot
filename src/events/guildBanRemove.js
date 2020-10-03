e259a6684846279a3b5f47fa9bf2b9acf3856b26
y3ll0wlife <y3ll0wlife@protonmail.com>
2020-10-03 15:32:20 +0200

    Removed unsused events

--- src/events/guildBanRemove.js
+++
@@ -1,6 +0,0 @@
-const SendLog = require("../handlers/SendLog");
-const Emotes = require("../emotes.json");
-
-module.exports = async (client, guild, user) => {
-  //SendLog.Mod_action(client, guild.id, `${Emotes.actions.unban} Unbanned **${user.username}**#${user.discriminator} \`\`(${user.id})\`\` by **Manual Unban**`, "");
-};
