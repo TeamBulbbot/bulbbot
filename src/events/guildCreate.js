a3be0d53cfe42a6178bca8f7a3642a07877c118c
y3ll0wlife <y3ll0wlife@protonmail.com>
2020-10-03 15:33:13 +0200

    unused arguments

--- src/events/guildCreate.js
+++ src/events/guildCreate.js
@@ -1,7 +1,7 @@
 const Database = require("../handlers/Database");
 const clc = require("cli-color");
 
-module.exports = async (client, guild) => {
+module.exports = async (_client, guild) => {
 	Database.AddGuild(guild);
 	console.log(clc.green("[+] Joined new server"));
 };
--- src/events/guildDelete.js
+++ src/events/guildDelete.js
@@ -1,7 +1,7 @@
 const Database = require("../handlers/Database");
 const clc = require("cli-color");
 
-module.exports = async (client, guild) => {
+module.exports = async (_client, guild) => {
 	Database.RemoveGuild(guild);
 	console.log(clc.yellow("[-] Removed from server"));
 };
