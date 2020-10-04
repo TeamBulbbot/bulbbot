6a8cffc9dba3b0f986372741d99aa68f705c14c5
y3ll0wlife <y3ll0wlife@protonmail.com>
2020-10-04 17:30:09 +0200

    Update message.js

--- src/events/message.js
+++ src/events/message.js
@@ -1,10 +1,21 @@
 const clc = require("cli-color");
 const Discord = require("discord.js");
-
 const Guild = require("../models/guild");
 const Database = require("../handlers/Database");
+const { Permissions } = require("discord.js");
 
 module.exports = async (client, message) => {
+	if (message.author.bot) return;
+	if (!message.guild) return;
+
+	if (
+		message.content === `<@${client.user.id}>` ||
+		message.content === `<@!${client.user.id}>`
+	)
+		return message.channel.send(
+			`Hi, the current prefix in **${message.guild.name}** is \`\`${prefix}\`\``
+		);
+
 	if (message.channel.type === "dm") {
 		const webhookClient = new Discord.WebhookClient(
 			process.env.WEBHOOK_ID,
@@ -36,17 +47,6 @@ module.exports = async (client, message) => {
 				prefix = process.env.PREFIX;
 			}
 
-			if (message.author.bot) return;
-			if (!message.guild) return;
-
-			if (
-				message.content === `<@${client.user.id}>` ||
-				message.content === `<@!${client.user.id}>`
-			)
-				return message.channel.send(
-					`Hi, the current prefix in **${message.guild.name}** is \`\`${prefix}\`\``
-				);
-
 			if (!message.content.startsWith(prefix)) return;
 			if (!message.member)
 				message.member = await message.guild.fetchMember(message);
@@ -60,9 +60,24 @@ module.exports = async (client, message) => {
 			if (!command) command = client.commands.get(client.aliases.get(cmd));
 
 			if (command) {
-				command.run(client, message, args);
+				const permissions = new Permissions(message.guild.me.permissions);
 
-				if (guild.trackAnalytics) await Database.CommandAnalyticsHandler(cmd);
+				if (permissions.has(command.clientPermissions)) {
+					command.run(client, message, args).catch((err) => {
+						console.error(err);
+						message.channel.send(
+							"**An error has occurred**, please check the the permssion level of the bot or try again later. If the issue persists contact the bot developers."
+						);
+					});
+
+					if (guild.trackAnalytics) await Database.CommandAnalyticsHandler(cmd);
+				} else {
+					return message.channel.send(
+						"**Missing required permissions** to run this command. I need the following permssions\n```" +
+							command.clientPermissions +
+							"```"
+					);
+				}
 			}
 		}
 	);
