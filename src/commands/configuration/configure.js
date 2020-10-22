c516fda5adc2ed4d0e2fab56aa6533e0a0bd421f
y3ll0wlife <y3ll0wlife@protonmail.com>
2020-10-22 18:11:54 +0200

    :wrench: Made some changes to the config command

--- src/commands/configuration/configure.js
+++ src/commands/configuration/configure.js
@@ -15,11 +15,11 @@ module.exports = {
 			return message.channel.send(":lock: Missing permission ``MANAGE_GUILD``"); // I know best has permssion lol
 		if (args[0] === undefined || args[0] === null)
 			return message.channel.send(
-				`${Emotes.actions.warn} Missing required argument \`\`setting\`\`\n${Emotes.other.tools} Correct usage of command: \`\`configure|cfg|setting|config <setting> <new value>\`\``
+				`${Emotes.actions.warn} Missing required argument \`\`setting\`\`\n${Emotes.other.tools} Correct usage of command: \`\`configure|cfg|setting|config <setting> <new value> | If you want to disable a command put disable inside of the <new value>\`\``
 			);
 		if (args[1] === undefined || args[1] === null)
 			return message.channel.send(
-				`${Emotes.actions.warn} Missing required argument \`\`new value\`\`\n${Emotes.other.tools} Correct usage of command: \`\`configure|cfg|setting|config <setting> <new value>\`\``
+				`${Emotes.actions.warn} Missing required argument \`\`new value\`\`\n${Emotes.other.tools} Correct usage of command: \`\`configure|cfg|setting|config <setting> <new value> | If you want to disable a command put disable inside of the <new value>\`\``
 			);
 		const setting = args[0].toLowerCase();
 		const newValue = args[1];
@@ -27,125 +27,34 @@ module.exports = {
 		switch (setting) {
 			// Guild configuration
 			case "prefix": // Change the bot prefix
-				Guild.Change_Prefix(message.guild.id, newValue);
-				message.channel.send(
-					`Successfully updated the prefix to \`\`${newValue}\`\` in **${message.guild.name}**`
-				);
+				Prefix(message, newValue);
 				break;
 
 			case "track_analytics":
-				if (newValue === "true" || newValue === "false") {
-					Guild.Track_Analytics(message.guild.id, newValue);
-					message.channel.send(
-						`Successfully update the state of tracking analytics to  \`\`${newValue}\`\` in **${message.guild.name}**`
-					);
-				} else
-					message.channel.send(
-						"Invalid value, allowed values ``true`` or ``false``"
-					);
+				Track_analytics(message, newValue);
 				break;
 
 			// Log configuration
 			case "mod_action":
-				if (
-					message.guild.channels.cache.get(newValue.replace(/\D/g, "")) ===
-					undefined
-				)
-					return message.channel.send(
-						`${Emotes.actions.warn} Invalid \`\`channel\`\``
-					);
-				Log.Change_Mod_Action(
-					client,
-					message.guild.id,
-					newValue.replace(/\D/g, "")
-				);
-				message.channel.send(
-					`Now logging \`\`mod actions\`\` in ${newValue} in **${message.guild.name}**`
-				);
+				Mod_action(client, message, newValue);
 				break;
 			case "message":
-				if (
-					message.guild.channels.cache.get(newValue.replace(/\D/g, "")) ===
-					undefined
-				)
-					return message.channel.send(
-						`${Emotes.actions.warn} Invalid \`\`channel\`\``
-					);
-				Log.Change_Message(
-					client,
-					message.guild.id,
-					newValue.replace(/\D/g, "")
-				);
-				message.channel.send(
-					`Now logging \`\`message logs\`\` in ${newValue} in **${message.guild.name}**`
-				);
+				Message(client, message, newValue);
 				break;
 			case "role":
 			case "role_update":
-				if (
-					message.guild.channels.cache.get(newValue.replace(/\D/g, "")) ===
-					undefined
-				)
-					return message.channel.send(
-						`${Emotes.actions.warn} Invalid \`\`channel\`\``
-					);
-				Log.Change_Role(client, message.guild.id, newValue.replace(/\D/g, ""));
-				message.channel.send(
-					`Now logging \`\`role updates\`\` in ${newValue} in **${message.guild.name}**`
-				);
+				Role_update(client, message, newValue);
 				break;
 			case "member":
 			case "member_update":
-				if (
-					message.guild.channels.cache.get(newValue.replace(/\D/g, "")) ===
-					undefined
-				)
-					return message.channel.send(
-						`${Emotes.actions.warn} Invalid \`\`channel\`\``
-					);
-				Log.Change_Member(
-					client,
-					message.guild.id,
-					newValue.replace(/\D/g, "")
-				);
-				message.channel.send(
-					`Now logging \`\`member updates\`\` in ${newValue} in **${message.guild.name}**`
-				);
+				Member_update(client, message, newValue);
 				break;
 			case "channel":
 			case "channel_update":
-				if (
-					message.guild.channels.cache.get(newValue.replace(/\D/g, "")) ===
-					undefined
-				)
-					return message.channel.send(
-						`${Emotes.actions.warn} Invalid \`\`channel\`\``
-					);
-				Log.Change_Channel(
-					client,
-					message.guild.id,
-					newValue.replace(/\D/g, "")
-				);
-				message.channel.send(
-					`Now logging \`\`channel updates\`\` in ${newValue} in **${message.guild.name}**`
-				);
+				Channel_update(client, message, newValue);
 				break;
 			case "join_leave":
-				if (
-					message.guild.channels.cache.get(newValue.replace(/\D/g, "")) ===
-					undefined
-				)
-					return message.channel.send(
-						`${Emotes.actions.warn} Invalid \`\`channel\`\``
-					);
-				Log.Change_Join_Leave(
-					client,
-					message.guild.id,
-					newValue.replace(/\D/g, "")
-				);
-				message.channel.send(
-					`Now logging \`\`join leave logs\`\` in ${newValue} in **${message.guild.name}**`
-				);
+				Join_leave(client, message, newValue);
 				break;
 
 			// Role configuration
@@ -250,9 +159,154 @@ module.exports = {
 
 			default:
 				message.channel.send(
-					`${Emotes.actions.warn} Invalid \`\`setting\`\`\n${Emotes.other.tools} Correct usage of command: \`\`configure|cfg|setting|config <setting> <new value>\`\`\n**Guild settings:** \`\`prefix\`\`, \`\`track_analytics\`\`\n**Logging settings:** \`\`mod_action\`\`, \`\`message\`\`, \`\`role|role_update\`\`, \`\`member|member_update\`\`, \`\`channel|channel_update\`\`, \`\`join_leave\`\`\n**Role settings:** \`\`admin\`\`, \`\`mod|moderator\`\`, \`\`mute\`\`, \`\`trusted\`\`\n**Settings:** \`\`lang|language\`\`, \`\`delete_server_invites\`\`, \`\`trusted_server_invites\`\`, \`\`allow_non_latin_usernames\`\`, \`\`dm_on_action\`\`, \`\`censored_words\`\`, \`\`delete_links\`\`, \`\`trusted_links\`\``
+					`${Emotes.actions.warn} Invalid \`\`setting\`\`\n${Emotes.other.tools} Correct usage of command: \`\`configure|cfg|setting|config <setting> <new value> | If you want to disable command put disable inside of the <new value>\`\`\n**Guild settings:** \`\`prefix\`\`, \`\`track_analytics\`\`\n**Logging settings:** \`\`mod_action\`\`, \`\`message\`\`, \`\`role|role_update\`\`, \`\`member|member_update\`\`, \`\`channel|channel_update\`\`, \`\`join_leave\`\`\n**Role settings:** \`\`admin\`\`, \`\`mod|moderator\`\`, \`\`mute\`\`, \`\`trusted\`\`\n**Settings:** \`\`lang|language\`\`, \`\`delete_server_invites\`\`, \`\`trusted_server_invites\`\`, \`\`allow_non_latin_usernames\`\`, \`\`dm_on_action\`\`, \`\`censored_words\`\`, \`\`delete_links\`\`, \`\`trusted_links\`\``
 				);
 				break;
 		}
 	},
 };
+
+// Helper functions
+function isChannel(message, channel) {
+	return (
+		message.guild.channels.cache.get(channel.replace(/\D/g, "")) !== undefined
+	);
+}
+
+// Guild configuration
+function Prefix(message, newValue) {
+	Guild.Change_Prefix(message.guild.id, newValue);
+	message.channel.send(
+		`Successfully updated the prefix to \`\`${newValue}\`\` in **${message.guild.name}**`
+	);
+}
+
+function Track_analytics(message, newValue) {
+	if (newValue === "true" || newValue === "false") {
+		Guild.Track_Analytics(message.guild.id, newValue);
+		message.channel.send(
+			`Successfully update the state of tracking analytics to  \`\`${newValue}\`\` in **${message.guild.name}**`
+		);
+	} else
+		message.channel.send("Invalid value, allowed values ``true`` or ``false``");
+}
+
+// Logging configuration
+function Mod_action(client, message, newValue) {
+	if (newValue === "disable") {
+		newValue = "";
+		message.channel.send(
+			`Stopped logging \`\`mod actions\`\` in **${message.guild.name}**`
+		);
+	} else if (!isChannel(message, newValue))
+		return message.channel.send(
+			`${Emotes.actions.warn} Invalid \`\`channel\`\``
+		);
+	else {
+		newValue = newValue.replace(/\D/g, "");
+		message.channel.send(
+			`Now logging \`\`mod actions\`\` in ${newValue} in **${message.guild.name}**`
+		);
+	}
+	Log.Change_Mod_Action(client, message.guild.id, newValue.replace(/\D/g, ""));
+}
+
+function Message(client, message, newValue) {
+	if (newValue === "disable") {
+		newValue = "";
+		message.channel.send(
+			`Stopped logging \`\`message\`\` in **${message.guild.name}**`
+		);
+	} else if (!isChannel(message, newValue))
+		return message.channel.send(
+			`${Emotes.actions.warn} Invalid \`\`channel\`\``
+		);
+	else {
+		newValue = newValue.replace(/\D/g, "");
+		message.channel.send(
+			`Now logging \`\`message\`\` in ${newValue} in **${message.guild.name}**`
+		);
+	}
+
+	Log.Change_Message(client, message.guild.id, newValue);
+}
+
+function Role_update(client, message, newValue) {
+	if (newValue === "disable") {
+		newValue = "";
+		message.channel.send(
+			`Stopped logging \`\`role updates\`\` in **${message.guild.name}**`
+		);
+	} else if (!isChannel(message, newValue))
+		return message.channel.send(
+			`${Emotes.actions.warn} Invalid \`\`channel\`\``
+		);
+	else {
+		newValue = newValue.replace(/\D/g, "");
+		message.channel.send(
+			`Now logging \`\`role updates\`\` in ${newValue} in **${message.guild.name}**`
+		);
+	}
+
+	Log.Change_Role(client, message.guild.id, newValue.replace(/\D/g, ""));
+}
+
+function Member_update(client, message, newValue) {
+	if (newValue === "disable") {
+		newValue = "";
+		message.channel.send(
+			`Stopped logging \`\`member updates\`\` in **${message.guild.name}**`
+		);
+	} else if (!isChannel(message, newValue))
+		return message.channel.send(
+			`${Emotes.actions.warn} Invalid \`\`channel\`\``
+		);
+	else {
+		newValue = newValue.replace(/\D/g, "");
+		message.channel.send(
+			`Now logging \`\`member updates\`\` in ${newValue} in **${message.guild.name}**`
+		);
+	}
+
+	Log.Change_Member(client, message.guild.id, newValue.replace(/\D/g, ""));
+}
+
+function Channel_update(client, message, newValue) {
+	if (newValue === "disable") {
+		newValue = "";
+		message.channel.send(
+			`Stopped logging \`\`channel updates\`\` in **${message.guild.name}**`
+		);
+	} else if (!isChannel(message, newValue))
+		return message.channel.send(
+			`${Emotes.actions.warn} Invalid \`\`channel\`\``
+		);
+	else {
+		newValue = newValue.replace(/\D/g, "");
+		message.channel.send(
+			`Now logging \`\`channel updates\`\` in ${newValue} in **${message.guild.name}**`
+		);
+	}
+
+	Log.Change_Channel(client, message.guild.id, newValue.replace(/\D/g, ""));
+}
+
+function Join_leave(client, message, newValue) {
+	if (newValue === "disable") {
+		newValue = "";
+		message.channel.send(
+			`Stopped logging \`\`join leave\`\` in **${message.guild.name}**`
+		);
+	} else if (!isChannel(message, newValue))
+		return message.channel.send(
+			`${Emotes.actions.warn} Invalid \`\`channel\`\``
+		);
+	else {
+		newValue = newValue.replace(/\D/g, "");
+		message.channel.send(
+			`Now logging \`\`join leave\`\` in ${newValue} in **${message.guild.name}**`
+		);
+	}
+
+	Log.Change_Join_Leave(client, message.guild.id, newValue.replace(/\D/g, ""));
+}
