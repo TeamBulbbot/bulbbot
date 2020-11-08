8811e7c26347d7fa06159e3127eb9aca90a90cbe
y3ll0wlife <y3ll0wlife@protonmail.com>
2020-11-08 16:20:42 +0100

    AAAAAAAAA I forgot it again

--- src/utils/helper/beautify.js
+++ src/utils/helper/beautify.js
@@ -29,10 +29,10 @@ module.exports = {
 			badges.push(emotes.badges.bug_hunter_green);
 		if ((bitfield & bughunter_gold) === bughunter_gold)
 			badges.push(emotes.badges.bug_hunter_gold);
-		if ((bitfield & earlysupport) === earlysupport)
-			badges.push(emotes.badges.eary_supporter);
 		if ((bitfield & botdeveloper) === botdeveloper)
 			badges.push(emotes.badges.verfied_bot_developer);
+		if ((bitfield & earlysupport) === earlysupport)
+			badges.push(emotes.badges.eary_supporter);
 
 		return badges.map((i) => `${i}`).join(" ");
 	},
