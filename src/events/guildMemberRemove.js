c076f42f7eb8708e3ea13823d18fcb4c7a78c17f
KlukCZ <l.neumajer@seznam.cz>
2020-11-27 19:40:31 +0100

    Fixed manual kick logging
    Oh baby it finally works (hopefully)

--- src/events/guildMemberRemove.js
+++ src/events/guildMemberRemove.js
@@ -19,12 +19,15 @@ module.exports = async (client, user) => {
 			})
 	)
 
-	/*const log = await user.guild.fetchAuditLogs({limit: 1, type: 'MEMBER_KICK'});
+	const log = await user.guild.fetchAuditLogs({limit: 1, type: 'MEMBER_KICK'});
 	const kickLog = log.entries.first();
 	if (!kickLog) return;
 
-	const { executor, reason } = kickLog;
+	let { executor, reason } = kickLog;
 	if (executor.id === client.user.id) return;
+	if (kickLog.createdAt < user.joinedAt) return;
+
+	if (reason === null) reason = "No reason given"
 
 	await Log.Mod_action(
 		client,
@@ -40,5 +43,5 @@ module.exports = async (client, user) => {
 				reason: reason
 			}),
 		""
-	)*/
+	)
 };
