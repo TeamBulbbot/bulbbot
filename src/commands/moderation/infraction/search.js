eec7bfb1d7298433a6daceabf255d3ac2df93385
KlukCZ <l.neumajer@seznam.cz>
2020-12-05 21:17:01 +0100

    Update search.js

--- src/commands/moderation/infraction/search.js
+++ src/commands/moderation/infraction/search.js
@@ -53,9 +53,10 @@ module.exports = {
 			},
 			async (err, infs) => {
 				for (let i = 0; i < infs.length; i++) {
+					let  moderator, user ;
 					try {
-						let user = await message.guild.member(infs[i].targetID).user;
-						let moderator = await message.guild.member(infs[i].moderatorID)
+						user = await message.guild.member(infs[i].targetID).user;
+						moderator = await message.guild.member(infs[i].moderatorID)
 							.user;
 					} catch (error) {
 						return message.channel.send(
