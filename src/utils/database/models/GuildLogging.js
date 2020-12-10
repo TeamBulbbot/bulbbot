20d09b151276b795c91f4c6968e768db8376c78a
y3ll0wlife <y3ll0wlife@protonmail.com>
2020-12-10 12:15:08 +0100

    Update GuildLogging.js

--- src/utils/database/models/GuildLogging.js
+++ src/utils/database/models/GuildLogging.js
@@ -4,27 +4,21 @@ module.exports = (sequelize) => {
     sequelize.define("GuildLogging", {
         ModAction: {
             type: DataTypes.STRING,
-            allowNull: true,
         },
         Message: {
             type: DataTypes.STRING,
-            allowNull: true,
         },
         Role: {
             type: DataTypes.STRING,
-            allowNull: true,
         },
         Member: {
             type: DataTypes.STRING,
-            allowNull: true,
         },
         Channel: {
             type: DataTypes.STRING,
-            allowNull: true,
         },
         JoinLeave: {
             type: DataTypes.STRING,
-            allowNull: true,
         },
     });
 };
