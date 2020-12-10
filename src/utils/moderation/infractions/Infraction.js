181345206fe892df0e28caf5da5294fcb0e917a6
y3ll0wlife <y3ll0wlife@protonmail.com>
2020-12-10 13:21:38 +0100

    DB: Infraction functions

---
+++ src/utils/moderation/infractions/Infraction.js
@@ -0,0 +1,15 @@
+const sequelize = require("../../database/connection");
+
+module.exports = {
+    async getAllInfs(guildId){
+
+    },
+
+    async getOffenderInfs(guildId){
+
+    },
+
+    async getModeratorInfs(guildId){
+
+    }
+}
