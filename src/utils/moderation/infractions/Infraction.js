69b5c325031e368a9aec5f018b9132c15f96fd5e
y3ll0wlife <y3ll0wlife@protonmail.com>
2020-12-10 18:14:25 +0100

    DB: Infractions get all infs, get offender infs, get mod infs

--- src/utils/moderation/infractions/Infraction.js
+++ src/utils/moderation/infractions/Infraction.js
@@ -2,14 +2,45 @@ const sequelize = require("../../database/connection");
 
 module.exports = {
     async getAllInfs(guildId){
+        const dbGuild = await sequelize.models.Guild.findOne({
+            where: {GuildId: guildId},
+            include: [
+                {model: sequelize.models.Infraction},
+            ]
+        })
 
+        return dbGuild.Infractions;
     },
 
-    async getOffenderInfs(guildId){
+    async getOffenderInfs(guildId, offenderId){
+        const dbGuild = await sequelize.models.Guild.findOne({
+            where: {GuildId: guildId},
+            include: [
+                {
+                    model: sequelize.models.Infraction,
+                    where: {
+                        TargetId: offenderId,
+                    }
+                },
+            ]
+        })
 
+        return dbGuild.Infractions;
     },
 
-    async getModeratorInfs(guildId){
+    async getModeratorInfs(guildId, moderatorId){
+        const dbGuild = await sequelize.models.Guild.findOne({
+            where: {GuildId: guildId},
+            include: [
+                {
+                    model: sequelize.models.Infraction,
+                    where: {
+                        ModeratorId: moderatorId,
+                    }
+                },
+            ]
+        })
 
+        return dbGuild.Infractions;
     }
 }
