db91b2dd68d3038bf3ec6b61f68aa720bf604e94
y3ll0wlife <y3ll0wlife@protonmail.com>
2020-12-10 12:14:46 +0100

    Update message.js

--- src/events/message.js
+++ src/events/message.js
@@ -1,5 +1,6 @@
 const Event = require("../structures/Event")
 const BulbBotUtils = require("./../utils/BulbBotUtils")
+const { getPrefix } = require("../utils/guilds/Handler")
 
 module.exports = class extends Event {
     constructor(...args) {
@@ -7,24 +8,18 @@ module.exports = class extends Event {
     }
 
     async run(message) {
+        this.client.prefix = getPrefix(message.guild)
+
+
         const mentionRegex = RegExp(`^<@!?${this.client.user.id}>`)
-        const mentionRegexPrefix = RegExp(`^<@!?${this.client.user.id}>`)
-
         if (!message.guild || message.author.bot) return
+        if (message.content.match(mentionRegex)) message.channel.send(`My prefix for **${message.guild.name}** is \`\`${this.client.prefix}\`\``)
+        if (!message.content.startsWith(this.client.prefix )) return
 
-        if (message.content.match(mentionRegex)) message.channel.send(`My prefix for ${message.guild.name} is ${this.client.prefix}`)
-
-        const prefix = message.content.match(mentionRegexPrefix) ?
-            message.content.match(mentionRegexPrefix)[0] : this.client.prefix
-
-        if (!message.content.startsWith(prefix)) return
-
-        const [cmd, ...args] = message.content.slice(prefix.length).trim().split(/ +/g)
+        const [cmd, ...args] = message.content.slice(this.client.prefix .length).trim().split(/ +/g)
 
         const command = this.client.commands.get(cmd.toLowerCase()) || this.client.commands.get(this.client.aliases.get(cmd.toLowerCase()))
         if (command) {
-
-            console.log(command.clearance)
             const userPermCheck = command.userPerms ? this.client.defaultPerms.add(command.userPerms) : this.client.defaultPerms
             if (userPermCheck) {
                 const missing = message.channel.permissionsFor(message.member).missing(userPermCheck)
