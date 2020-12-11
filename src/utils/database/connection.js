19459592aa776d9f8405102d0091234cc80fe329
y3ll0wlife <y3ll0wlife@protonmail.com>
2020-12-12 00:25:45 +0100

    hehehe Kluk have fun

--- src/utils/database/connection.js
+++ src/utils/database/connection.js
@@ -9,7 +9,7 @@ config({
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
 	host: process.env.DB_HOST,
 	dialect: "postgres",
-	logging: false,
+	logging: console.log,
 	logQueryParameters: true,
 	benchmark: true,
 	pool: {
