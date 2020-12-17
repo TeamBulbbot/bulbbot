7d8c42e1a197304b0a922756b1638dc8cb029734
y3ll0wlife <y3ll0wlife@protonmail.com>
2020-12-17 22:45:07 +0100

    new command: avatar

--- src/commands/Moderation/infraction.js
+++ src/commands/Moderation/infraction.js
@@ -1,4 +1,4 @@
-const Command = require("./../../structures/Command");
+const Command = require("../../structures/Command");
 const { createInfraction } = require("../../utils/InfractionUtils");
 
 module.exports = class extends (
