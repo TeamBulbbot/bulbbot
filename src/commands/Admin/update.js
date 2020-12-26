28e30acf168d317fb370f8f0036727db444e8578
y3ll0wlife <y3ll0wlife@protonmail.com>
2020-12-26 21:47:28 +0100

    new command: update

---
+++ src/commands/Admin/update.js
@@ -0,0 +1,27 @@
+const Command = require("../../structures/Command");
+const shell = require("shelljs");
+const path = require("path");
+
+module.exports = class extends (
+	Command
+) {
+	constructor(...args) {
+		super(...args, {
+			description: "Pulls the latest code from master and reloads the bot",
+			category: "Admin",
+			usage: "!update",
+			devOnly: true,
+		});
+	}
+
+	async run(message, args) {
+		const p = path.resolve(__dirname, "");
+		shell.cd(`${p}/../../../`);
+		const resp = shell.exec("git pull");
+
+		return message.channel.send(resp.stdout, { code: "txt" });
+
+		// TODO
+		// figure out a way to restart the program
+	}
+};
