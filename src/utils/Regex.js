04bccc277267c64c8740983f1224cf314af75225
y3ll0wlife <y3ll0wlife@protonmail.com>
2020-12-28 22:37:51 +0100

    I own the world's worst thesaurus. Not only is it awful, it's awful.

--- src/utils/Regex.js
+++ src/utils/Regex.js
@@ -4,3 +4,4 @@ exports.UserMention = /<@?!?[0-9>]+/g;
 exports.UserMentionStrict = /<@?!?[0-9>]+|[0-9>]{17,}/g;
 exports.RoleMention = /<@&[0-9>]+/g;
 exports.EveryoneHereMention = /@everyone|@here+/g;
+exports.Emote = /:[0-9>]{17,}>+/g;
