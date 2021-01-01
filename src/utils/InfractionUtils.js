e647ef75fdcfacce28ee1585ba8005a9dabd63a9
KlukCZ <l.neumajer@seznam.cz>
2021-01-01 14:44:38 +0100

    Javadoc goes brrrrrrrr

--- src/utils/InfractionUtils.js
+++ src/utils/InfractionUtils.js
@@ -1,19 +1,18 @@
 const sequelize = require("./database/connection");
-const { Op } = require("sequelize")
 
 module.exports = {
 	/**
 	 * Creates an infraction for the provided guild
 	 *
-	 * @param guildId               Guild ID where the Infraction is being stored from
+	 * @param guildId               Guild ID where the Infraction is being stored in
 	 * @param action                Mod action type
-	 * @param active                false, permanent, unix
+	 * @param active                false, true, Unix
 	 * @param reason                Reason specified by the responsible moderator
 	 * @param target                User receiving the infraction
 	 * @param targetId              ID of the user receiving the infraction
 	 * @param moderator             Moderator responsible for the infraction
 	 * @param moderatorId           ID of the responsible moderator
-	 * @returns InfId               The infraction id of the infraction created
+	 * @returns InfId               The ID of the created infraction
 	 */
 	createInfraction: async (guildId, action, active, reason, target, targetId, moderator, moderatorId) => {
 		const dbGuild = await sequelize.models.Guild.findOne({
@@ -63,7 +62,7 @@ module.exports = {
 	 * Returns all Infractions stored for the specified guild
 	 *
 	 * @param guildId           Guild ID
-	 * @returns {Promise<*>}    Returned infraction array
+	 * @returns {Promise<*[]|*>}    Returned infraction array
 	 */
 	getAllInfractions: async guildId => {
 		const dbGuild = await sequelize.models.Guild.findOne({
@@ -82,7 +81,7 @@ module.exports = {
 	 *
 	 * @param guildId           Guild ID
 	 * @param offenderId        Searched user ID marked as offender in the database
-	 * @returns {Promise<*>}    Returned infraction array
+	 * @returns {Promise<*[]|*>}    Returned infraction array
 	 */
 	getOffenderInfractions: async (guildId, offenderId) => {
 		const dbGuild = await sequelize.models.Guild.findOne({
@@ -108,7 +107,7 @@ module.exports = {
 	 *
 	 * @param guildId           Guild ID
 	 * @param moderatorId       Searched user ID marked as moderator in the database
-	 * @returns {Promise<*>}    Returned infraction array
+	 * @returns {Promise<*[]|*>}    Returned infraction array
 	 */
 	getModeratorInfractions: async (guildId, moderatorId) => {
 		const dbGuild = await sequelize.models.Guild.findOne({
@@ -128,6 +127,14 @@ module.exports = {
 		return dbGuild.Infractions.reverse();
 	},
 
+	/**
+	 * Sets the "active" column for the selected infraction to "active" in
+	 * the database
+	 *
+	 * @param infId		Infraction ID
+	 * @param active	The active value, either a boolean or a Unix timestamp
+	 * @returns {Promise<void>}
+	 */
 	setActive: async (infId, active) => {
 		const dbInf = await sequelize.models.Infraction.findOne({
 			where: { id: infId },
@@ -137,7 +144,13 @@ module.exports = {
 		await dbInf.save();
 	},
 
-	getActive: async (infId) => {
+	/**
+	 * Returns the "active" value for the selected infraction
+	 *
+	 * @param infId		Infraction ID
+	 * @returns {Promise<{allowNull: boolean, type: *}|*>}		Returned value, either a boolean or a Unix timestamp
+	 */
+	getActive: async infId => {
 		const dbInf = await sequelize.models.Infraction.findOne({
 			where: { id: infId },
 		});
@@ -145,6 +158,13 @@ module.exports = {
 		return dbInf.Active;
 	},
 
+	/**
+	 * Returns the latest Mute infraction for the provided Guild and User
+	 *
+	 * @param guildId		Selected Guild ID
+	 * @param offenderId	The selected User ID
+	 * @returns {Promise<*[]|*>}		The latest Mute infraction stored in the database
+	 */
 	getLatestMute: async (guildId, offenderId) => {
 		const dbGuild = await sequelize.models.Guild.findOne({
 			where: { GuildId: guildId },
@@ -153,7 +173,7 @@ module.exports = {
 					model: sequelize.models.Infraction,
 					where: {
 						TargetId: offenderId,
-						Action: "Mute"
+						Action: "Mute",
 					},
 				},
 			],
