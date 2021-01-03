4a56f70d01e92c0f318597cb5276d140ce439447
y3ll0wlife <y3ll0wlife@protonmail.com>
2021-01-03 17:22:00 +0100

    lol

--- src/structures/Config.js
+++
@@ -1,7 +0,0 @@
-const fs = require("fs");
-const yaml = require("js-yaml");
-
-let fileContents = fs.readFileSync(`${__dirname}/../config.yml`, "utf8");
-let data = yaml.safeLoad(fileContents);
-
-global.config = data;
