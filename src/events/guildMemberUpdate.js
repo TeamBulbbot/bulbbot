bed1c42ea01f87067b8a6faac3d1605269ed8a76
y3ll0wlife <y3ll0wlife@protonmail.com>
2021-02-28 18:05:29 +0100

    Update guildMemberUpdate.js

--- src/events/guildMemberUpdate.js
+++ src/events/guildMemberUpdate.js
@@ -1,11 +1,24 @@
 const Event = require("../structures/Event");
+const { SendEventLog } = require("../utils/moderation/log");
 
 module.exports = class extends Event {
 	constructor(...args) {
 		super(...args, {});
 	}
 
-	run(oldMember, newMember) {
-		// TODO
+	async run(oldMember, newMember) {
+		let change = "";
+
+		if (oldMember._roles.length !== newMember._roles.length)
+			oldMember._roles.length < newMember._roles.length ? (change = "newrole") : (change = "removedrole");
+		else if (oldMember.nickname !== newMember.nickname) change = "nickname";
+		else return;
+
+		console.log(oldMember);
+
+		let message = "";
+		let role = "";
+
+		await SendEventLog(this.client, newMember.guild, "member", change);
 	}
 };
