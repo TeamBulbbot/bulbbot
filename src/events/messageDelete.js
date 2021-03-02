520900dd8da6f4d9e07db08dc0f93ec9e87f77bc
y3ll0wlife <y3ll0wlife@protonmail.com>
2021-03-02 19:09:57 +0100

    Update messageDelete.js

--- src/events/messageDelete.js
+++ src/events/messageDelete.js
@@ -10,10 +10,11 @@ module.exports = class extends Event {
 	run(message) {
 		if (message.author.id === this.client.user.id) return;
 
-		let msg = `Message from **${message.author.tag}** \`${message.author.id}\` was deleted in <#${message.channel.id}>\n\`ID (channel-message): ${message.channel.id}-${message.id}\`\n`;
+		let msg = `Message from **${message.author.tag}** \`${message.author.id}\` was deleted in <#${message.channel.id}>\n\`ID (channel-message): ${message.channel.id}-${message.id}\`\n**C:** `;
 
 		if (message.attachments.first()) msg += `**A:** ${message.attachments.first().proxyURL}\n`;
-		if (message.content) msg += `**C:** ${Util.cleanContent(message.content, message)}`;
+		if (message.content) msg += `${Util.cleanContent(message.content, message)}`;
+		if (message.embeds.length !== 0) msg += "[Embed]";
 
 		if (msg.length >= 2000) {
 			SendEventLog(this.client, message.guild, "message", msg.substring(0, 1500));
