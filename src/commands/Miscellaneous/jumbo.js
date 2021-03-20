16bfe94ea6fd7ef1371bb7edd3a4df10c286c795
y3ll0wlife <y3ll0wlife@protonmail.com>
2021-03-20 17:13:08 +0100

    Fixed an issue with the jumbo command

--- src/commands/Miscellaneous/jumbo.js
+++ src/commands/Miscellaneous/jumbo.js
@@ -2,7 +2,7 @@ const Command = require("../../structures/Command");
 const sharp = require("sharp");
 const fs = require("fs");
 const emojiUnicode = require("emoji-unicode");
-const { CustomEmote, NonDigits } = require("../../utils/Regex");
+const { CustomEmote, GetEverythingAfterColon } = require("../../utils/Regex");
 const axios = require("axios");
 
 module.exports = class extends Command {
@@ -19,74 +19,80 @@ module.exports = class extends Command {
 	}
 
 	async run(message, args) {
-		const size = 250;
-		const imgPath = [];
+		try {
+			const size = 250;
+			const imgPath = [];
 
-		sharp.cache({ files: 0 });
+			sharp.cache({ files: 0 });
 
-		await sharp({
-			create: {
-				width: size * args.length + 1,
-				height: size,
-				channels: 4,
-				background: { r: 0, g: 0, b: 0, alpha: 0 },
-			},
-		})
-			.png()
-			.toFile(`src/files/jumbo/${message.author.id}-${message.guild.id}.png`);
+			await sharp({
+				create: {
+					width: size * args.length + 1,
+					height: size,
+					channels: 4,
+					background: { r: 0, g: 0, b: 0, alpha: 0 },
+				},
+			})
+				.png()
+				.toFile(`src/files/jumbo/${message.author.id}-${message.guild.id}.png`);
 
-		for (let i = 0; i < args.length; i++) {
-			let emote = args[i];
-			let url;
+			for (let i = 0; i < args.length; i++) {
+				let emote = args[i];
+				let url;
 
-			emote = emote.match(CustomEmote);
+				emote = emote.match(CustomEmote);
 
-			if (emote === null) {
-				url = `https://cdnjs.cloudflare.com/ajax/libs/twemoji/13.0.1/svg/${emojiUnicode(args[i]).split(" ")[0]}.svg`;
-			} else {
-				emote = emote[0].substring(1).slice(0, -1);
-				url = `https://cdn.discordapp.com/emojis/${emote.replace(NonDigits, "")}.png?v=1`;
-			}
+				if (emote === null) {
+					url = `https://cdnjs.cloudflare.com/ajax/libs/twemoji/13.0.1/svg/${emojiUnicode(args[i]).split(" ")[0]}.svg`;
+				} else {
+					emote = emote[0].substring(1).slice(0, -1);
+					emote = emote.match(GetEverythingAfterColon);
 
-			await axios
-				.get(url, { responseType: "arraybuffer" })
-				.then(async res => {
-					return await sharp(res.data, { density: 2400 })
-						.png()
-						.resize(size, size)
-						.toFile(`src/files/jumbo/${i}-${message.author.id}-${message.guild.id}.png`);
-				})
-				.catch(err => {
-					console.error(err);
+					url = `https://cdn.discordapp.com/emojis/${emote[0]}.png?v=1`;
+				}
+
+				await axios
+					.get(url, { responseType: "arraybuffer" })
+					.then(async res => {
+						return await sharp(res.data, { density: 2400 })
+							.png()
+							.resize(size, size)
+							.toFile(`src/files/jumbo/${i}-${message.author.id}-${message.guild.id}.png`);
+					})
+					.catch(err => {
+						console.error(err);
+					});
+
+				imgPath.push({
+					input: `src/files/jumbo/${i}-${message.author.id}-${message.guild.id}.png`,
+					gravity: "southeast",
+					top: size,
+					left: size * i,
+					density: 2400,
+					premultiplied: true,
 				});
-
-			imgPath.push({
-				input: `src/files/jumbo/${i}-${message.author.id}-${message.guild.id}.png`,
-				gravity: "southeast",
-				top: size,
-				left: size * i,
-				density: 2400,
-				premultiplied: true,
-			});
-		}
-
-		await sharp(`src/files/jumbo/${message.author.id}-${message.guild.id}.png`)
-			.composite(imgPath)
-			.png()
-			.toFile(`src/files/jumbo/final-${message.author.id}-${message.guild.id}.png`);
-
-		await message.channel.send({
-			files: [`src/files/jumbo/final-${message.author.id}-${message.guild.id}.png`],
-		});
-
-		fs.unlinkSync(`src/files/jumbo/${message.author.id}-${message.guild.id}.png`);
-		fs.unlinkSync(`src/files/jumbo/final-${message.author.id}-${message.guild.id}.png`);
-		for (let i = 0; i < args.length; i++) {
-			try {
-				fs.unlinkSync(`src/files/jumbo/${i}-${message.author.id}-${message.guild.id}.png`);
-			} catch (error) {
-				continue;
 			}
+
+			await sharp(`src/files/jumbo/${message.author.id}-${message.guild.id}.png`)
+				.composite(imgPath)
+				.png()
+				.toFile(`src/files/jumbo/final-${message.author.id}-${message.guild.id}.png`);
+
+			await message.channel.send({
+				files: [`src/files/jumbo/final-${message.author.id}-${message.guild.id}.png`],
+			});
+
+			fs.unlinkSync(`src/files/jumbo/${message.author.id}-${message.guild.id}.png`);
+			fs.unlinkSync(`src/files/jumbo/final-${message.author.id}-${message.guild.id}.png`);
+			for (let i = 0; i < args.length; i++) {
+				try {
+					fs.unlinkSync(`src/files/jumbo/${i}-${message.author.id}-${message.guild.id}.png`);
+				} catch (error) {
+					continue;
+				}
+			}
+		} catch (error) {
+			return message.channel.send(await this.client.bulbutils.translate("jumbo_invalid_emoji"));
 		}
 	}
 };
--- src/utils/Regex.js
+++ src/utils/Regex.js
@@ -7,6 +7,7 @@ exports.ChannelMention = /<#(\d+)>/g;
 exports.EveryoneHereMention = /@everyone|@here/g;
 exports.Emoji = /(\p{ExtPict}(?:\p{EComp}(?:\p{ExtPict}|\p{EMod}|\p{EBase}))*\p{EComp}?|\d\u{FE0F}\u{20E3}|[\u{1F1E6}-\u{1F1FF}][\u{1F1E6}-\u{1F1FF}]?)/gu; // Matches all Unicode emoji, shouldn't match anything else
 exports.CustomEmote = /<(?:a)?:([a-zA-Z0-9_]{2,32}):(\d{17,})>/g; // Matches custom emotes
+exports.GetEverythingAfterColon = /(?<=\:)[^:]*$/;
 exports.ReasonImage = /((?:https?:\/\/)[a-z0-9]+(?:[-.][a-z0-9]+)*\.[a-z]{2,5}(?::[0-9]{1,5})?(?:\/[^ \n<>]*)\.(?:png|apng|jpg|gif))/g;
 
 exports.AutoMod_INVITE = /(?:https?:\/\/)?(?:www\.)?discord(?:\.gg|\.me|(?:app)?\.com\/invite)\/([A-Za-z0-9-]+)/g;
