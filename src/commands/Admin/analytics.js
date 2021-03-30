dcc479ee945e694593c8495d3d035ae701029cd9
y3ll0wlife <y3ll0wlife@protonmail.com>
2021-03-30 02:16:42 +0200

    Update analytics.js

--- src/commands/Admin/analytics.js
+++ src/commands/Admin/analytics.js
@@ -48,20 +48,3 @@ module.exports = class extends Command {
 		return message.channel.send(embed);
 	}
 };
-
-function delta() {
-	const cpus = os.cpus();
-
-	return cpus.map(cpu => {
-		const times = cpu.times;
-		return {
-			tick: Object.keys(times)
-				.filter(time => time !== "idle")
-				.reduce((tick, time) => {
-					tick += times[time];
-					return tick;
-				}, 0),
-			idle: times.idle,
-		};
-	});
-}
