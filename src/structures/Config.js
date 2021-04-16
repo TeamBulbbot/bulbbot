be9216a08b75a2e6b2e8e2770faa4bd32394cd76
y3ll0wlife <y3ll0wlife@protonmail.com>
2021-04-17 00:28:34 +0200

    Update Config.js

--- src/structures/Config.js
+++ src/structures/Config.js
@@ -2,6 +2,6 @@ const fs = require("fs");
 const yaml = require("js-yaml");
 
 let fileContents = fs.readFileSync(`${__dirname}/../config.yml`, "utf8");
-let data = yaml.safeLoad(fileContents);
+let data = yaml.load(fileContents);
 
 global.config = data;
