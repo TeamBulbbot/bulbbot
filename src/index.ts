470d244f8f47161015ee2cee4264d80b60ae608c
y3ll0wlife <y3ll0wlife@protonmail.com>
2021-06-26 17:01:03 +0200

    fixed bug, regarding .env

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import * as Config from "./structures/Config";
 import BulbBotClient from "./structures/BulbBotClient";
-import * as DotEnv from "dotenv";
+import * as env from "dotenv";
 import { sequelize } from "./utils/database/connection";
-DotEnv.config();
+env.config({ path: `${__dirname}/.env` });
 
 const config = {
 	token: process.env.TOKEN,
