caf53fb5d3c7599652e8a99dd8eac0304731e33f
y3ll0wlife <y3ll0wlife@protonmail.com>
2021-06-27 18:05:05 +0200

    Update sync.ts

--- src/utils/database/sync.ts
+++ src/utils/database/sync.ts
@@ -1,15 +1,19 @@
-import { sequelize } from "./connection"
+import { sequelize } from "./connection";
 
 async function sync(): Promise<void> {
-    const start: number = new Date().getTime();
-    console.log("[DB] Starting database sync...");
+	const start: number = new Date().getTime();
+	console.log("[DB] Starting database sync...");
 
-    //await sequelize.sync();
-    await sequelize.sync({ alter: true });
+	await sequelize.sync({
+		alter: true,
+		//force: true,
+		logging: true,
+		benchmark: true,
+	});
 
-    const end: number = new Date().getTime();
+	const end: number = new Date().getTime();
 
-    console.log(`\n[DB] Database sync done, it took ${end - start} ms!`);
+	console.log(`\n[DB] Database sync done, it took ${end - start} ms!`);
 }
 
 sync();
