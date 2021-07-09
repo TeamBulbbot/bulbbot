d49bf1f6bfea8db742da0ff283f919ac1b4839c6
KlukCZ <L.Neumajer@seznam.cz>
2021-07-09 13:52:29 +0200

    Updated the AutoMod table
    AutoMod now accepts ignore channels, roles and users

--- src/utils/database/models/AutoMod.ts
+++ src/utils/database/models/AutoMod.ts
@@ -1,63 +1,75 @@
 import { DataTypes, Sequelize } from "sequelize";
 
 export default function (sequelize: Sequelize): void {
-    sequelize.define(
-        "automod",
-        {
-            enabled: {
-                type: DataTypes.BOOLEAN,
-                defaultValue: false,
-            },
-            websiteWhitelist: {
-                type: DataTypes.ARRAY(DataTypes.STRING),
-                defaultValue: [],
-            },
-            punishmentWebsite: {
-                type: DataTypes.STRING,
-            },
-            inviteWhitelist: {
-                type: DataTypes.ARRAY(DataTypes.STRING),
-                defaultValue: [],
-            },
-            punishmentInvites: {
-                type: DataTypes.STRING,
-            },
-            wordBlacklist: {
-                type: DataTypes.ARRAY(DataTypes.STRING),
-                defaultValue: [],
-            },
-            wordBlacklistToken: {
-                type: DataTypes.ARRAY(DataTypes.STRING),
-                defaultValue: [],
-            },
-            punishmentWords: {
-                type: DataTypes.STRING,
-            },
-            limitMentions: {
-                type: DataTypes.INTEGER,
-                defaultValue: 0,
-            },
-            punishmentMentions: {
-                type: DataTypes.STRING,
-            },
-          timeoutMentions: {
-            type: DataTypes.INTEGER,
-            defaultValue: 15,
-          },
-            limitMessages: {
-                type: DataTypes.INTEGER,
-                defaultValue: 0,
-            },
-            punishmentMessages: {
-                type: DataTypes.STRING,
-            },
-          timeoutMessages: {
-            type: DataTypes.INTEGER,
-            defaultValue: 10,
-          },
-        },
-        {
-            timestamps: false,
-        },
-    );
+	sequelize.define(
+		"automod",
+		{
+			enabled: {
+				type: DataTypes.BOOLEAN,
+				defaultValue: false,
+			},
+			websiteWhitelist: {
+				type: DataTypes.ARRAY(DataTypes.STRING),
+				defaultValue: [],
+			},
+			punishmentWebsite: {
+				type: DataTypes.STRING,
+			},
+			inviteWhitelist: {
+				type: DataTypes.ARRAY(DataTypes.STRING),
+				defaultValue: [],
+			},
+			punishmentInvites: {
+				type: DataTypes.STRING,
+			},
+			wordBlacklist: {
+				type: DataTypes.ARRAY(DataTypes.STRING),
+				defaultValue: [],
+			},
+			wordBlacklistToken: {
+				type: DataTypes.ARRAY(DataTypes.STRING),
+				defaultValue: [],
+			},
+			punishmentWords: {
+				type: DataTypes.STRING,
+			},
+			limitMentions: {
+				type: DataTypes.INTEGER,
+				defaultValue: 0,
+			},
+			punishmentMentions: {
+				type: DataTypes.STRING,
+			},
+			timeoutMentions: {
+				type: DataTypes.INTEGER,
+				defaultValue: 15,
+			},
+			limitMessages: {
+				type: DataTypes.INTEGER,
+				defaultValue: 0,
+			},
+			punishmentMessages: {
+				type: DataTypes.STRING,
+			},
+			timeoutMessages: {
+				type: DataTypes.INTEGER,
+				defaultValue: 10,
+			},
+			ignoreChannels: {
+				type: DataTypes.ARRAY(DataTypes.STRING),
+				defaultValue: [],
+			},
+			ignoreRoles: {
+				type: DataTypes.ARRAY(DataTypes.STRING),
+				defaultValue: [],
+			},
+			ignoreUsers: {
+				type: DataTypes.ARRAY(DataTypes.STRING),
+				defaultValue: [],
+			},
+		},
+		{
+			timestamps: false,
+		},
+	);
 }
