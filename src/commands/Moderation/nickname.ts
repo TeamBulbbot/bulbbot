e9fff2285127c50306f62db94776cc08e4d85850
Walker Gray <55608093+wakfi@users.noreply.github.com>
2021-07-27 23:59:40 -0700

    Fix undefined

--- src/commands/Moderation/nickname.ts
+++ src/commands/Moderation/nickname.ts
@@ -30,7 +30,7 @@ export default class extends Command {
 		const targetID: Snowflake = match[1] ?? match[2];
 		const target: GuildMember = <GuildMember>message.guild?.member(targetID);
 		const nickmatch: RegExpMatchArray = <RegExpMatchArray>QuoteMarked.exec(argString);
-		const nickname: string = (nickmatch ? nickmatch[1] : args[1]).trim() ?? "";
+		const nickname: string = (nickmatch ? nickmatch[1] : args[1])?.trim() ?? "";
 		const reason: string =
 			args
 				.slice(1 + nickname.split(" ").length)
