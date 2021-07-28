08c3bf4f4fba41e84d6c84f0f3680e55cec3796c
Walker Gray <55608093+wakfi@users.noreply.github.com>
2021-07-27 22:51:07 -0700

    Add channelCreate/channelDelete events

---
+++ src/events/guild/channels/channelDelete.ts
@@ -0,0 +1,28 @@
+import { DMChannel, GuildChannel } from "discord.js";
+import Event from "../../../structures/Event";
+import LoggingManager from "../../../utils/managers/LoggingManager";
+
+const loggingManager: LoggingManager = new LoggingManager();
+
+export default class extends Event {
+	constructor(...args) {
+		// @ts-ignore
+        super(...args, {});
+	}
+
+	async run(channel: DMChannel | GuildChannel) {
+		if (!channel) return;
+        if (!(channel instanceof GuildChannel)) return;
+
+		loggingManager.sendEventLog(
+			this.client,
+			channel.guild,
+			"channel",
+			await this.client.bulbutils.translate("event_channel_delete", channel.guild.id, {
+				channel_name: channel.name,
+				channel_id: channel.id,
+				channel_type: channel.type,
+			}),
+		);
+	}
+};
