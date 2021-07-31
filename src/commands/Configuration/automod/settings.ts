34286404d5e02a022bb77d54f17173e7564599c6
Walker Gray <55608093+wakfi@users.noreply.github.com>
2021-07-30 22:26:30 -0700

    Add automod settings subcommand
    Change string-concatenation based approach to string-array based, to take advantage of array.join() for separator insertion

--- src/commands/Configuration/automod/settings.ts
+++ src/commands/Configuration/automod/settings.ts
@@ -1,73 +1,86 @@
-import BulbBotClient from "../../../structures/BulbBotClient";
-import { Message, MessageEmbed, Snowflake } from "discord.js";
-import { embedColor } from "../../../Config";
-import { AutoModConfiguration } from "../../../utils/types/AutoModConfiguration";
+import { Message, MessageEmbed } from "discord.js";
 import DatabaseManager from "../../../utils/managers/DatabaseManager";
-import * as Emotes from "../../../emotes.json";
+import SubCommand from "../../../structures/SubCommand";
+import Command from "../../../structures/Command";
+import { AutoModConfiguration } from "../../../utils/types/AutoModConfiguration";
+import { embedColor } from "../../../Config";
+import Emotes from "../../../emotes.json";
 
 const databaseManager: DatabaseManager = new DatabaseManager();
 
-export default async function (client: BulbBotClient, message: Message): Promise<void> {
-	const dbGuild: AutoModConfiguration = await databaseManager.getAutoModConfig(<Snowflake>message.guild?.id);
+export default class extends SubCommand {
+	constructor(...args: any) {
+		// @ts-ignore
+		super(...args, {
+			name: "enable",
+			clearance: 75,
+			maxArgs: 0,
+			usage: "!automod enable",
+		});
+	}
 
-	let roles: string = "";
-	let channels: string = "";
-	let users: string = "";
+	public async run(message: Message, parent: Command, args: string[]): Promise<void> {
+		const dbGuild: AutoModConfiguration = await databaseManager.getAutoModConfig(message.guild!.id);
 
-	dbGuild.ignoreRoles.length ? dbGuild.ignoreRoles.forEach(role => (roles += `<@&${role}> `)) : (roles = "None");
-	dbGuild.ignoreChannels.length ? dbGuild.ignoreChannels.forEach(channel => (channels += `<#${channel}> `)) : (channels = "None");
-	dbGuild.ignoreUsers.length ? dbGuild.ignoreUsers.forEach(user => (users += `<@${user}> `)) : (users = "None");
+		const roles: string[] = [];
+		const channels: string[] = [];
+		const users: string[] = [];
 
-	let description: string = "";
+		dbGuild.ignoreRoles.length ? dbGuild.ignoreRoles.forEach(role => (roles.push(`<@&${role}>`))) : (roles.push("None"));
+		dbGuild.ignoreChannels.length ? dbGuild.ignoreChannels.forEach(channel => (channels.push(`<#${channel}>`))) : (channels.push("None"));
+		dbGuild.ignoreUsers.length ? dbGuild.ignoreUsers.forEach(user => (users.push(`<@${user}>`))) : (users.push("None"));
 
-	description += await client.bulbutils.translate("automod_settings_enabled", message.guild?.id, {
-		part: dbGuild.enabled ? Emotes.other.SWITCHON : Emotes.other.SWITCHOFF,
-	});
+		const description: string[] = [];
 
-	description += await client.bulbutils.translate("automod_settings_websites", message.guild?.id, {
-		part: dbGuild.punishmentWebsite !== null ? `\`${dbGuild.punishmentWebsite}\`` : Emotes.other.SWITCHOFF,
-		item: dbGuild.websiteWhitelist.length ? dbGuild.websiteWhitelist.join(" ") : "None",
-	});
+		description.push(await this.client.bulbutils.translate("automod_settings_enabled", message.guild?.id, {
+			part: dbGuild.enabled ? Emotes.other.SWITCHON : Emotes.other.SWITCHOFF,
+		}));
 
-	description += await client.bulbutils.translate("automod_settings_invites", message.guild?.id, {
-		part: dbGuild.punishmentInvites !== null ? `\`${dbGuild.punishmentInvites}\`` : Emotes.other.SWITCHOFF,
-		item: dbGuild.inviteWhitelist.length ? dbGuild.inviteWhitelist.join(" ") : "None",
-	});
+		description.push(await this.client.bulbutils.translate("automod_settings_websites", message.guild?.id, {
+			part: dbGuild.punishmentWebsite !== null ? `\`${dbGuild.punishmentWebsite}\`` : Emotes.other.SWITCHOFF,
+			item: dbGuild.websiteWhitelist.length ? dbGuild.websiteWhitelist.join(" ") : "None",
+		}));
 
-	description += await client.bulbutils.translate("automod_settings_words", message.guild?.id, {
-		part: dbGuild.punishmentWords !== null ? `\`${dbGuild.punishmentWords}\`` : Emotes.other.SWITCHOFF,
-		item: dbGuild.wordBlacklist.length ? dbGuild.wordBlacklist.join(" ") : "None",
-		count: dbGuild.wordBlacklistToken.length ? dbGuild.wordBlacklistToken.join(" ") : "None",
-	});
+		description.push(await this.client.bulbutils.translate("automod_settings_invites", message.guild?.id, {
+			part: dbGuild.punishmentInvites !== null ? `\`${dbGuild.punishmentInvites}\`` : Emotes.other.SWITCHOFF,
+			item: dbGuild.inviteWhitelist.length ? dbGuild.inviteWhitelist.join(" ") : "None",
+		}));
 
-	description += await client.bulbutils.translate("automod_settings_mentions", message.guild?.id, {
-		part: dbGuild.punishmentMentions ? `\`${dbGuild.punishmentMentions}\`` : Emotes.other.SWITCHOFF,
-		amount: dbGuild.limitMentions,
-		limit: dbGuild.timeoutMentions / 1000,
-	});
+		description.push(await this.client.bulbutils.translate("automod_settings_words", message.guild?.id, {
+			part: dbGuild.punishmentWords !== null ? `\`${dbGuild.punishmentWords}\`` : Emotes.other.SWITCHOFF,
+			item: dbGuild.wordBlacklist.length ? dbGuild.wordBlacklist.join(" ") : "None",
+			count: dbGuild.wordBlacklistToken.length ? dbGuild.wordBlacklistToken.join(" ") : "None",
+		}));
 
-	description += await client.bulbutils.translate("automod_settings_messages", message.guild?.id, {
-		part: dbGuild.punishmentMessages ? `\`${dbGuild.punishmentMessages}\`` : Emotes.other.SWITCHOFF,
-		amount: dbGuild.limitMessages,
-		limit: dbGuild.timeoutMessages / 1000,
-	});
+		description.push(await this.client.bulbutils.translate("automod_settings_mentions", message.guild?.id, {
+			part: dbGuild.punishmentMentions ? `\`${dbGuild.punishmentMentions}\`` : Emotes.other.SWITCHOFF,
+			amount: dbGuild.limitMentions,
+			limit: dbGuild.timeoutMentions / 1000,
+		}));
 
-	description += await client.bulbutils.translate("automod_settings_ignored", message.guild?.id, {
-		role: roles,
-		channel_id: channels,
-		target_tag: users,
-	});
+		description.push(await this.client.bulbutils.translate("automod_settings_messages", message.guild?.id, {
+			part: dbGuild.punishmentMessages ? `\`${dbGuild.punishmentMessages}\`` : Emotes.other.SWITCHOFF,
+			amount: dbGuild.limitMessages,
+			limit: dbGuild.timeoutMessages / 1000,
+		}));
 
-	const embed: MessageEmbed = new MessageEmbed()
-		.setColor(embedColor)
-		.setAuthor(
-			await client.bulbutils.translate("automod_settings_header", message.guild?.id, {
-				guild_name: message.guild?.name,
-			}),
-			<string>message.guild?.iconURL({ dynamic: true }),
-		)
-		.setDescription(description)
-		.setFooter(await client.bulbutils.translate("automod_settings_footer", message.guild?.id, {}), "https://cdn.discordapp.com/emojis/833770837575860305.png?v=1");
+		description.push(await this.client.bulbutils.translate("automod_settings_ignored", message.guild?.id, {
+			role: roles.join(" "),
+			channel_id: channels.join(" "),
+			target_tag: users.join(" "),
+		}));
 
-	await message.channel.send(embed);
+		const embed: MessageEmbed = new MessageEmbed()
+			.setColor(embedColor)
+			.setAuthor(
+				await this.client.bulbutils.translate("automod_settings_header", message.guild?.id, {
+					guild_name: message.guild?.name,
+				}),
+				message.guild!.iconURL({ dynamic: true }) ?? undefined,
+			)
+			.setDescription(description.join("\n\n"))
+			.setFooter(await this.client.bulbutils.translate("automod_settings_footer", message.guild?.id, {}), "https://cdn.discordapp.com/emojis/833770837575860305.png?v=1");
+
+		await message.channel.send(embed);
+	}
 }
