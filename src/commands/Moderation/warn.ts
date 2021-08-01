dae9b1c4ec683592f2fd00fd594b36c38f59d416
KlukCZ <L.Neumajer@seznam.cz>
2021-08-01 19:08:46 +0200

    Moved warn.ts to new translator

--- src/commands/Moderation/warn.ts
+++ src/commands/Moderation/warn.ts
@@ -31,7 +31,8 @@ export default class extends Command {
 		//Checks if reason or target are null and if the target is actionable
 		if (!target) {
 			await message.channel.send(
-				await this.client.bulbutils.translate("global_user_not_found", message.guild?.id, {
+				await this.client.bulbutils.translateNew("global_not_found", message.guild?.id, {
+					type: await this.client.bulbutils.translateNew("global_not_found_types.member", message.guild?.id, {}),
 					arg_expected: "member:Member",
 					arg_provided: args[0],
 					usage: this.usage,
@@ -39,7 +40,7 @@ export default class extends Command {
 			);
 			return;
 		}
-		if (!reason) reason = await this.client.bulbutils.translate("global_no_reason", message.guild?.id, {});
+		if (!reason) reason = await this.client.bulbutils.translateNew("global_no_reason", message.guild?.id, {});
 
 		if (await this.client.bulbutils.resolveUserHandle(message, this.client.bulbutils.checkUser(message, target), target.user)) return;
 
@@ -49,12 +50,10 @@ export default class extends Command {
 			<string>message.guild?.id,
 			target,
 			<GuildMember>message.member,
-			await this.client.bulbutils.translate("global_mod_action_log", message.guild?.id, {
-				action: "Warned",
-				moderator_tag: message.author.tag,
-				moderator_id: message.author.id,
-				target_tag: target.user.tag,
-				target_id: target.user.id,
+			await this.client.bulbutils.translateNew("global_mod_action_log", message.guild?.id, {
+				action: await this.client.bulbutils.translateNew("mod_action_types.warn", message.guild?.id, {}),
+				moderator: message.author,
+				target: target.user,
 				reason,
 			}),
 			reason,
@@ -62,12 +61,11 @@ export default class extends Command {
 
 		//Sends the respond message
 		await message.channel.send(
-			await this.client.bulbutils.translate("warn_success", message.guild?.id, {
-				user_name: target.user.username,
-				user_discriminator: target.user.discriminator,
-				user_id: target.user.id,
-				reason: reason,
-				infractionId: infID,
+			await this.client.bulbutils.translateNew("action_success", message.guild?.id, {
+				action: await this.client.bulbutils.translateNew("mod_action_types.warn", message.guild?.id, {}),
+				target: target.user,
+				reason,
+				infraction_id: infID,
 			}),
 		);
 	}
