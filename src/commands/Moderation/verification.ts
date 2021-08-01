0ad700d1fd3e168c445390bd1378d1204a38e176
KlukCZ <L.Neumajer@seznam.cz>
2021-08-01 19:20:28 +0200

    Moved verification.ts to new translator

--- src/commands/Moderation/verification.ts
+++ src/commands/Moderation/verification.ts
@@ -1,10 +1,8 @@
 import Command from "../../structures/Command";
-import { Guild, Message } from "discord.js";
+import { Message } from "discord.js";
 import { NonDigits } from "../../utils/Regex";
 import LoggingManager from "../../utils/managers/LoggingManager";
-
-const loggingManager: LoggingManager = new LoggingManager();
-
+new LoggingManager();
 export default class extends Command {
 	constructor(...args) {
 		// @ts-ignore
@@ -24,22 +22,19 @@ export default class extends Command {
 
 	public async run(message: Message, args: string[]): Promise<void | Message> {
 		const level: number = parseInt(args[0].replace(NonDigits, ""));
-		if (!level && level !== 0) return message.channel.send(await this.client.bulbutils.translate("verification_non_integer", message.guild?.id));
-		if (message.guild?.features.includes("COMMUNITY") && level === 0) return message.channel.send(await this.client.bulbutils.translate("verification_community_zero", message.guild.id));
+		if (!level && level !== 0)
+			return message.channel.send(
+				await this.client.bulbutils.translateNew("global_cannot_convert", message.guild?.id, {
+					arg_provided: args[0],
+					arg_expected: "verification:int",
+					usage: this.usage,
+				}),
+			);
+		if (message.guild?.features.includes("COMMUNITY") && level === 0) return message.channel.send(await this.client.bulbutils.translateNew("verification_community_zero", message.guild.id, {}));
 
-		if (level > 4 || level < 0) return message.channel.send(await this.client.bulbutils.translate("verification_level_error", message.guild?.id));
+		if (level > 4 || level < 0) return message.channel.send(await this.client.bulbutils.translateNew("verification_level_error", message.guild?.id, {}));
 
 		await message.guild?.setVerificationLevel(level);
-		await message.channel.send(await this.client.bulbutils.translate("verification_level_success", message.guild?.id, { level }));
-
-		await loggingManager.sendServerEventLog(
-			this.client,
-			<Guild>message.guild,
-			await this.client.bulbutils.translate("global_server_event", message.guild?.id, {
-				moderator_tag: message.author.tag,
-				moderator_id: message.author.id,
-				part: await this.client.bulbutils.translate("verification_log", message.guild?.id, { level }),
-			}),
-		);
+		await message.channel.send(await this.client.bulbutils.translateNew("verification_level_success", message.guild?.id, { level }));
 	}
 }
