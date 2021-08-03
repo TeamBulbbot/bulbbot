e28dc8366aefeced05ba718b7cb4aaf580010bf9
KlukCZ <L.Neumajer@seznam.cz>
2021-08-03 11:45:30 +0200

    Moved multiunban.ts to new translator

--- src/commands/Moderation/multiunban.ts
+++ src/commands/Moderation/multiunban.ts
@@ -28,15 +28,19 @@ export default class extends Command {
 		const targets: RegExpMatchArray = <RegExpMatchArray>args.slice(0).join(" ").match(UserMentionAndID);
 		let reason: string = args.slice(targets.length).join(" ").replace(UserMentionAndID, "");
 
-		if (reason === "") reason = await this.client.bulbutils.translate("global_no_reason", message.guild?.id);
+		if (reason === "") reason = await this.client.bulbutils.translateNew("global_no_reason", message.guild?.id, {});
 		let fullList: string = "";
 
 		if (targets!!.length <= 1) {
-			await message.channel.send(await this.client.bulbutils.translate("multiunban_targets_too_few", message.guild?.id));
+			await message.channel.send(
+				await this.client.bulbutils.translateNew("action_multi_less_than_2", message.guild?.id, {
+					action: await this.client.bulbutils.translateNew("action_multi_types.unban", message.guild?.id, {}),
+				}),
+			);
 			return await this.client.commands.get("unban")!.run(message, args);
 		}
 
-		message.channel.send(await this.client.bulbutils.translate("global_loading", message.guild?.id)).then(msg => {
+		message.channel.send(await this.client.bulbutils.translateNew("global_loading", message.guild?.id, {})).then(msg => {
 			msg.delete({ timeout: (args.length - 0.5) * massCommandSleep });
 		});
 
@@ -49,7 +53,14 @@ export default class extends Command {
 			try {
 				target = await this.client.users.fetch(targets[i].replace(NonDigits, ""));
 			} catch (error) {
-				await message.channel.send(await this.client.bulbutils.translate("global_user_not_found", message.guild?.id));
+				await message.channel.send(
+					await this.client.bulbutils.translateNew("global_not_found", message.guild?.id, {
+						type: await this.client.bulbutils.translateNew("global_not_found_types.user", message.guild?.id, {}),
+						arg_expected: "user:User",
+						arg_provided: targets[i],
+						usage: this.usage,
+					}),
+				);
 			}
 
 			infID = await infractionsManager.unban(
@@ -57,12 +68,10 @@ export default class extends Command {
 				<Guild>message.guild,
 				target,
 				<GuildMember>message.member,
-				await this.client.bulbutils.translate("global_mod_action_log", message.guild?.id, {
-					action: "Unban",
-					moderator_tag: message.author.tag,
-					moderator_id: message.author.id,
-					target_tag: target.tag,
-					target_id: target.id,
+				await this.client.bulbutils.translateNew("global_mod_action_log", message.guild?.id, {
+					action: await this.client.bulbutils.translateNew("mod_action_types.unban", message.guild?.id, {}),
+					moderator: message.author,
+					target,
 					reason,
 				}),
 				reason,
@@ -72,7 +81,8 @@ export default class extends Command {
 		}
 
 		return message.channel.send(
-			await this.client.bulbutils.translate("multiunban_success", message.guild?.id, {
+			await this.client.bulbutils.translateNew("action_success_multi", message.guild?.id, {
+				action: await this.client.bulbutils.translateNew("mod_action_types.unban", message.guild?.id, {}),
 				full_list: fullList,
 				reason,
 			}),
