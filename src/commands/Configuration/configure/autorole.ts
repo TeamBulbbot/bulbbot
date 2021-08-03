ce026a828524f9d95eef1fee3184d349d6f78b75
KlukCZ <L.Neumajer@seznam.cz>
2021-08-03 17:53:52 +0200

    Moved configure auto_role to new translator

--- src/commands/Configuration/configure/autorole.ts
+++ src/commands/Configuration/configure/autorole.ts
@@ -22,19 +22,29 @@ export default class extends SubCommand {
 
 	public async run(message: Message, parent: Command, args: string[]): Promise<void | Message> {
 		let role: Role | null = null;
-		if (args[1] !== "disable") {
-			role = <Role>message.guild?.roles.cache.get(args[1].replace(NonDigits, ""));
-			if (message.guild?.me?.roles.highest && message.guild?.me.roles.highest.rawPosition < role!!.rawPosition)
-				return message.channel.send(await this.client.bulbutils.translate("config_mute_unable_to_manage", message.guild.id));
-		}
-		if (role === undefined) return message.channel.send(await this.client.bulbutils.translate("config_mute_invalid_role", message.guild?.id));
+		role = <Role>message.guild?.roles.cache.get(args[1].replace(NonDigits, ""));
 
-		if (role !== null) {
+		if (role === undefined && args[1] !== "disable")
+			return message.channel.send(
+				await this.client.bulbutils.translateNew("global_not_found", message.guild?.id, {
+					type: await this.client.bulbutils.translateNew("global_not_found_types.role", message.guild?.id, {}),
+					arg_provided: args[1],
+					arg_expected: "role:Role",
+					usage: this.usage,
+				}),
+			);
+
+		if (args[1] !== "disable") {
+			if (message.guild?.me?.roles.highest && message.guild?.me.roles.highest.rawPosition < role!!.rawPosition)
+				return message.channel.send(await this.client.bulbutils.translateNew("config_mute_unable_to_manage", message.guild.id, {}));
+		}
+
+		if (role !== undefined) {
 			await databaseManager.setAutoRole(<Snowflake>message.guild?.id, role.id);
-			return message.channel.send(await this.client.bulbutils.translate("config_autorole_success", message.guild?.id));
+			return message.channel.send(await this.client.bulbutils.translateNew("config_autorole_success", message.guild?.id, { role: role.name }));
 		} else {
 			await databaseManager.setAutoRole(<Snowflake>message.guild?.id, null);
-			return message.channel.send(await this.client.bulbutils.translate("config_autorole_disable", message.guild?.id));
+			return message.channel.send(await this.client.bulbutils.translateNew("config_autorole_disable", message.guild?.id, {}));
 		}
 	}
 }
