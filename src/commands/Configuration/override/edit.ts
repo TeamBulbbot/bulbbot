7066232ff8294ed4b2bdea64e04583b5546582b1
KlukCZ <L.Neumajer@seznam.cz>
2021-08-05 16:03:56 +0200

    Moved configure override edit to new translator

--- src/commands/Configuration/override/edit.ts
+++ src/commands/Configuration/override/edit.ts
@@ -18,42 +18,73 @@ export default class extends SubCommand {
 		});
 	}
 
-	async run( message: Message, args: string[]): Promise<void | Message> {
+	async run(message: Message, args: string[]): Promise<void | Message> {
 		const part = args[0];
 		const name = args[1];
 		let clearance = Number(args[2]);
 
 		if (isNaN(clearance))
-			return message.channel.send(await this.client.bulbutils.translate("override_edit_non_number", message.guild?.id, { clearance: args[2] }));
-		if (clearance <= 0) return message.channel.send(await this.client.bulbutils.translate("override_edit_less_than_0", message.guild?.id));
-		if (clearance >= 100) return message.channel.send(await this.client.bulbutils.translate("override_edit_more_than_100", message.guild?.id));
-		if (clearance > this.client.userClearance)
-			return message.channel.send(await this.client.bulbutils.translate("override_edit_higher_than_yourself", message.guild?.id));
+			return message.channel.send(
+				await this.client.bulbutils.translateNew("global_not_found", message.guild?.id, {
+					type: await this.client.bulbutils.translateNew("global_not_found_types.int", message.guild?.id, {}),
+					arg_expected: "clearance:int",
+					arg_provided: args[2],
+					usage: this.usage,
+				}),
+			);
+		if (clearance <= 0) return message.channel.send(await this.client.bulbutils.translateNew("override_clearance_less_than_0", message.guild?.id, {}));
+		if (clearance >= 100) return message.channel.send(await this.client.bulbutils.translateNew("override_clearance_more_than_100", message.guild?.id, {}));
+		if (clearance > this.client.userClearance) return message.channel.send(await this.client.bulbutils.translateNew("override_clearance_higher_than_self", message.guild?.id, {}));
 
 		switch (part) {
 			case "role":
 				const roleID = name.replace(NonDigits, "");
 				const rTemp = message.guild?.roles.cache.get(roleID);
-				if (rTemp === undefined) return message.channel.send(await this.client.bulbutils.translate("override_edit_invalid_role", message.guild?.id));
+				if (rTemp === undefined)
+					return message.channel.send(
+						await this.client.bulbutils.translateNew("global_not_found", message.guild?.id, {
+							type: await this.client.bulbutils.translateNew("global_not_found_types.role", message.guild?.id, {}),
+							arg_expected: "role:Role",
+							arg_provided: args[1],
+							usage: this.usage,
+						}),
+					);
 
-				if (await clearanceManager.getRoleOverride(<Snowflake>message.guild?.id, rTemp.id) === undefined)
-					return message.channel.send(await this.client.bulbutils.translate("override_edit_non_existent_override_role", message.guild?.id));
-				await clearanceManager.editRoleOverride(<Snowflake>message.guild?.id, roleID, clearance)
+				if ((await clearanceManager.getRoleOverride(<Snowflake>message.guild?.id, rTemp.id)) === undefined)
+					return message.channel.send(await this.client.bulbutils.translateNew("override_nonexistent_role", message.guild?.id, { role: rTemp.name }));
+				await clearanceManager.editRoleOverride(<Snowflake>message.guild?.id, roleID, clearance);
 				break;
 
 			case "command":
 				const command = this.client.commands.get(name.toLowerCase()) || this.client.commands.get(<string>this.client.aliases.get(name.toLowerCase()));
 				if (command === undefined)
-					return message.channel.send(await this.client.bulbutils.translate("override_edit_invalid_command", message.guild?.id, { command: name }));
+					return message.channel.send(
+						await this.client.bulbutils.translateNew("global_not_found", message.guild?.id, {
+							type: await this.client.bulbutils.translateNew("global_not_found_types.cmd", message.guild?.id, {}),
+							arg_expected: "command:string",
+							arg_provided: args[1],
+							usage: this.usage,
+						}),
+					);
 
-				if (await clearanceManager.getCommandOverride(<Snowflake>message.guild?.id, name) === undefined)
-					return message.channel.send(await this.client.bulbutils.translate("override_edit_non_existent_override_command", message.guild?.id));
-				await clearanceManager.editCommandOverride(<Snowflake>message.guild?.id, command.name, clearance)
+				if ((await clearanceManager.getCommandOverride(<Snowflake>message.guild?.id, name)) === undefined)
+					return message.channel.send(
+						await this.client.bulbutils.translateNew("override_nonexistent_command", message.guild?.id, {
+							command: name,
+						}),
+					);
+				await clearanceManager.editCommandOverride(<Snowflake>message.guild?.id, command.name, clearance);
 
 				break;
 			default:
-				return message.channel.send(await this.client.bulbutils.translate("override_edit_invalid_part", message.guild?.id));
+				return message.channel.send(
+					await this.client.bulbutils.translateNew("event_message_args_missing_list", message.guild?.id, {
+						arg_expected: "part:string",
+						argument: args[0],
+						argument_list: "`role`, `command`",
+					}),
+				);
 		}
-		await message.channel.send(await this.client.bulbutils.translate("override_edit_success", message.guild?.id, { clearance }));
+		await message.channel.send(await this.client.bulbutils.translateNew("override_edit_success", message.guild?.id, { clearance }));
 	}
 }
