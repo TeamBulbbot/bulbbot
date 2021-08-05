ef8fa2196dd79be592ba4468eaea9f223eb9a848
KlukCZ <L.Neumajer@seznam.cz>
2021-08-05 16:17:14 +0200

    Moved configure override delete to new translator

--- src/commands/Configuration/override/remove.ts
+++ src/commands/Configuration/override/remove.ts
@@ -19,29 +19,35 @@ export default class extends SubCommand {
 		});
 	}
 
-	async run( message: Message, args: string[]): Promise<void | Message> {
+	async run(message: Message, args: string[]): Promise<void | Message> {
 		const part = args[0];
 		const name = args[1];
 
 		switch (part) {
 			case "role":
 				if (!(await clearanceManager.getRoleOverride(<Snowflake>message.guild?.id, name.replace(NonDigits, ""))))
-					return message.channel.send(await this.client.bulbutils.translate("override_remove_non_existent_override_role", message.guild?.id));
+					return message.channel.send(await this.client.bulbutils.translateNew("override_nonexistent_role", message.guild?.id, { role: name }));
 
 				await clearanceManager.deleteRoleOverride(<Snowflake>message.guild?.id, name.replace(NonDigits, ""));
 				break;
 
 			case "command":
 				const cTemp = this.client.commands.get(name.toLowerCase()) || this.client.commands.get(<string>this.client.aliases.get(name.toLowerCase()));
-				if (!cTemp || !(await clearanceManager.getCommandOverride(<Snowflake>message.guild?.id, cTemp.name)))
-					return message.channel.send(await this.client.bulbutils.translate("override_remove_non_existent_override_command", message.guild?.id));
+				if (!cTemp || cTemp.name === undefined || !(await clearanceManager.getCommandOverride(<Snowflake>message.guild?.id, cTemp.name)))
+					return message.channel.send(await this.client.bulbutils.translateNew("override_nonexistent_command", message.guild?.id, { command: name }));
 
 				await clearanceManager.deleteCommandOverride(<Snowflake>message.guild?.id, cTemp.name);
 				break;
 			default:
-				return message.channel.send(await this.client.bulbutils.translate("override_create_invalid_part", message.guild?.id));
+				return message.channel.send(
+					await this.client.bulbutils.translateNew("event_message_args_missing_list", message.guild?.id, {
+						arg_expected: "part:string",
+						argument: args[0],
+						argument_list: "`role`, `command`",
+					}),
+				);
 		}
 
-		await message.channel.send(await this.client.bulbutils.translate("override_remove_success", message.guild?.id));
+		await message.channel.send(await this.client.bulbutils.translateNew("override_remove_success", message.guild?.id, {}));
 	}
 }
