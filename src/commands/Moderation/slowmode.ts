71f03a5591b7bc6a0973282ac30dbad6797c2cf0
KlukCZ <L.Neumajer@seznam.cz>
2021-08-09 12:17:36 +0200

    Moved slowmode.ts to new translator

--- src/commands/Moderation/slowmode.ts
+++ src/commands/Moderation/slowmode.ts
@@ -1,11 +1,8 @@
 import Command from "../../structures/Command";
-import { Guild, Message, Snowflake, TextChannel } from "discord.js";
+import { Message, Snowflake, TextChannel } from "discord.js";
 import { NonDigits } from "../../utils/Regex";
 import parse from "parse-duration";
-import LoggingManager from "../../utils/managers/LoggingManager";
 import BulbBotClient from "../../structures/BulbBotClient";
-
-const loggingManager: LoggingManager = new LoggingManager();
 
 export default class extends Command {
 	constructor(client: BulbBotClient, name: string) {
@@ -32,7 +29,8 @@ export default class extends Command {
 
 		if (!channel)
 			return message.channel.send(
-				await this.client.bulbutils.translate("global_channel_not_found", message.guild?.id, {
+				await this.client.bulbutils.translateNew("global_not_found", message.guild?.id, {
+					type: await this.client.bulbutils.translateNew("global_not_found_types.channel", message.guild?.id, {}),
 					arg_expected: "channel:Channel",
 					arg_provided: args[0],
 					usage: this.usage,
@@ -42,48 +40,33 @@ export default class extends Command {
 		if (args.length === 1) duration = <number>parse(args[0]);
 		else duration = <number>parse(args[1]);
 
-		if (duration < <number>parse("0s") || duration === null) return message.channel.send(await this.client.bulbutils.translate("slowmode_invalid_0s", message.guild?.id));
-		if (duration > <number>parse("6h")) return message.channel.send(await this.client.bulbutils.translate("slowmode_invalid_6h", message.guild?.id));
+		if (duration < <number>parse("0s") || duration === null) return message.channel.send(await this.client.bulbutils.translateNew("duration_invalid_0s", message.guild?.id, {}));
+		if (duration > <number>parse("6h")) return message.channel.send(await this.client.bulbutils.translateNew("duration_invalid_6h", message.guild?.id, {}));
 
-		const before: number = channel.rateLimitPerUser;
 		try {
 			await channel.setRateLimitPerUser(duration / 1000);
 		} catch (error) {
 			return await message.channel.send(
-				await this.client.bulbutils.translate("slowmode_missing_perms", message.guild?.id, {
+				await this.client.bulbutils.translateNew("slowmode_missing_perms", message.guild?.id, {
 					channel,
 				}),
 			);
 		}
 
-		if (duration === parse("0s")) await message.channel.send(await this.client.bulbutils.translate("slowmode_success_remove", message.guild?.id, { channel }));
+		if (duration === parse("0s")) await message.channel.send(await this.client.bulbutils.translateNew("slowmode_success_remove", message.guild?.id, { channel }));
 		else if (args.length === 1)
 			await message.channel.send(
-				await this.client.bulbutils.translate("slowmode_success", message.guild?.id, {
+				await this.client.bulbutils.translateNew("slowmode_success", message.guild?.id, {
 					channel,
 					slowmode: args[0],
 				}),
 			);
 		else
 			await message.channel.send(
-				await this.client.bulbutils.translate("slowmode_success", message.guild?.id, {
+				await this.client.bulbutils.translateNew("slowmode_success", message.guild?.id, {
 					channel,
 					slowmode: args[1],
 				}),
 			);
-
-		await loggingManager.sendServerEventLog(
-			this.client,
-			<Guild>message.guild,
-			await this.client.bulbutils.translate("global_channel_event", message.guild?.id, {
-				moderator_tag: message.author.tag,
-				moderator_id: message.author.id,
-				channel_id: channel.id,
-				part: await this.client.bulbutils.translate("slowmode_log", message.author.id, {
-					before,
-					after: duration / 1000,
-				}),
-			}),
-		);
 	}
 }
