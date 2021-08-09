826e300d14f4d7c556a19824c48232e780dd070c
KlukCZ <L.Neumajer@seznam.cz>
2021-08-09 17:05:02 +0200

    Moved channelCreate.ts to new translator

--- src/events/guild/channels/channelCreate.ts
+++ src/events/guild/channels/channelCreate.ts
@@ -7,21 +7,13 @@ const loggingManager: LoggingManager = new LoggingManager();
 export default class extends Event {
 	constructor(...args) {
 		// @ts-ignore
-        super(...args, {});
+		super(...args, {});
 	}
 
 	async run(channel: DMChannel | GuildChannel) {
 		if (!channel) return;
-        if (!(channel instanceof GuildChannel)) return;
+		if (!(channel instanceof GuildChannel)) return;
 
-		loggingManager.sendEventLog(
-			this.client,
-			channel.guild,
-			"channel",
-			await this.client.bulbutils.translate("event_channel_create", channel.guild.id, {
-				channel_id: channel.id,
-				channel_type: channel.type,
-			}),
-		);
+		await loggingManager.sendEventLog(this.client, channel.guild, "channel", await this.client.bulbutils.translateNew("event_channel_create", channel.guild.id, { channel }));
 	}
-};
+}
