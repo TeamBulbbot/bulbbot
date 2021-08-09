1bca3b9b3f6518cd1c39639de3a33eb9d053bdc2
KlukCZ <L.Neumajer@seznam.cz>
2021-08-09 12:30:51 +0200

    Moved nickname.ts to new translator

--- src/commands/Moderation/nickname.ts
+++ src/commands/Moderation/nickname.ts
@@ -27,7 +27,15 @@ export default class extends Command {
 		const argString: string = args.slice(1).join(" ");
 		UserMentionAndID.lastIndex = 0;
 		const match: RegExpMatchArray = <RegExpMatchArray>UserMentionAndID.exec(args[0]);
-		if (!match) return message.channel.send(await this.client.bulbutils.translate("global_user_not_found", message.guild?.id));
+		if (!match)
+			return message.channel.send(
+				await this.client.bulbutils.translateNew("global_not_found", message.guild?.id, {
+					type: await this.client.bulbutils.translateNew("global_not_found_types.member", message.guild?.id, {}),
+					arg_expected: "member:Member",
+					arg_provided: args[0],
+					usage: this.usage,
+				}),
+			);
 		const targetID: Snowflake = match[1] ?? match[2];
 		const target: GuildMember = <GuildMember>message.guild?.member(targetID);
 		const nickmatch: RegExpMatchArray = <RegExpMatchArray>QuoteMarked.exec(argString);
@@ -36,31 +44,21 @@ export default class extends Command {
 			args
 				.slice(1 + nickname.split(" ").length)
 				.join(" ")
-				.trim() || (await this.client.bulbutils.translate("global_no_reason", message.guild?.id));
-		if (!target) return message.channel.send(await this.client.bulbutils.translate("global_user_not_found", message.guild?.id));
+				.trim() || (await this.client.bulbutils.translateNew("global_no_reason", message.guild?.id, {}));
+		if (!target)
+			return message.channel.send(
+				await this.client.bulbutils.translateNew("global_not_found", message.guild?.id, {
+					type: await this.client.bulbutils.translateNew("global_not_found_types.member", message.guild?.id, {}),
+					arg_expected: "member:Member",
+					arg_provided: args[0],
+					usage: this.usage,
+				}),
+			);
 		if (await this.client.bulbutils.resolveUserHandle(message, await this.client.bulbutils.checkUser(message, target), target.user)) return;
 
-		if (nickname.length > 32)
-			return message.channel.send(
-				await this.client.bulbutils.translate("nickname_too_long", message.guild?.id, {
-					nick_length: nickname.length.toString(),
-				}),
-			);
-		if (!nickname && !target.nickname)
-			return message.channel.send(
-				await this.client.bulbutils.translate("already_no_nickname", message.guild?.id, {
-					target_tag: target.user.tag,
-					target_id: target.user.id,
-				}),
-			);
-		if (nickname === target.nickname)
-			return message.channel.send(
-				await this.client.bulbutils.translate("already_has_nickname", message.guild?.id, {
-					target_tag: target.user.tag,
-					target_id: target.user.id,
-					nick_new: target.nickname,
-				}),
-			);
+		if (nickname.length > 32) return message.channel.send(await this.client.bulbutils.translateNew("nickname_too_long", message.guild?.id, { length: nickname.length.toString() }));
+		if (!nickname && !target.nickname) return message.channel.send(await this.client.bulbutils.translateNew("already_no_nickname", message.guild?.id, { target: target.user }));
+		if (nickname === target.nickname) return message.channel.send(await this.client.bulbutils.translateNew("already_has_nickname", message.guild?.id, { target: target.user, nickname: target.nickname }));
 
 		const nickOld: string = target.nickname || target.user.username;
 		let infID: number;
@@ -70,12 +68,10 @@ export default class extends Command {
 				<Guild>message.guild,
 				target,
 				<GuildMember>message.member,
-				await this.client.bulbutils.translate("global_mod_action_log", message.guild?.id, {
+				await this.client.bulbutils.translateNew("global_mod_action_log", message.guild?.id, {
 					action: nickname ? "Nickname changed" : "Nickname removed",
-					moderator_tag: message.author.tag,
-					moderator_id: message.author.id,
-					target_tag: target.user.tag,
-					target_id: target.user.id,
+					moderator: message.author,
+					target: target.user,
 					reason,
 				}),
 				reason,
@@ -84,24 +80,17 @@ export default class extends Command {
 			);
 		} catch (e) {
 			console.error(e.stack);
-			return message.channel.send(
-				await this.client.bulbutils.translate("change_nick_fail", message.guild?.id, {
-					target_tag: target.user.tag,
-					target_id: target.user.id,
-				}),
-			);
+			return message.channel.send(await this.client.bulbutils.translateNew("change_nick_fail", message.guild?.id, { target: target.user }));
 		}
 
 		return message.channel.send(
-			await this.client.bulbutils.translate(nickname ? "change_nick_success" : "remove_nick_success", message.guild?.id, {
-				target_tag: target.user.tag,
-				target_id: target.user.id,
+			await this.client.bulbutils.translateNew(nickname ? "change_nick_success" : "remove_nick_success", message.guild?.id, {
+				target: target.user,
 				nick_old: nickOld,
 				nick_new: nickname,
-				moderator_tag: message.author.tag,
-				moderator_id: message.author.id,
+				moderator: message.author,
 				reason,
-				infractionId: infID,
+				infraction_id: infID,
 			}),
 		);
 	}
