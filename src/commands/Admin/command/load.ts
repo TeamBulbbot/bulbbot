3ef62899a6de92be9ab7caf0fb3ba30b1881471e
Walker Gray <55608093+wakfi@users.noreply.github.com>
2021-08-14 23:34:05 -0700

    Add hot reload commands

---
+++ src/commands/Admin/command/load.ts
@@ -0,0 +1,94 @@
+import Command from "../../../structures/Command";
+import SubCommand from "../../../structures/SubCommand";
+import { Message } from "discord.js";
+import BulbBotClient from "../../../structures/BulbBotClient";
+import glob from "glob";
+import { promisify } from "util";
+import path from "path";
+
+const globAsync = promisify(glob);
+
+export default class extends SubCommand {
+	private isClass(input: any): boolean {
+		return typeof input === "function" && typeof input.prototype === "object" && input.toString().substring(0, 5) === "class";
+	}
+
+	constructor(client: BulbBotClient, parent: Command) {
+		super(client, parent, {
+			name: "load",
+			aliases: ["l"],
+			minArgs: 1,
+			maxArgs: -1,
+			argList: ["command:Command|SubCommand"],
+			usage: "<command>",
+		});
+	}
+
+	public async run(message: Message, args: string[]): Promise<void | Message> {
+		const cmd: string = args[0].toLowerCase();
+		let cmdFile: string = "";
+		let command: Command | undefined = Command.resolve(this.client, args);
+		let dirPath: string;
+
+		if(args.length > 1) {
+			if(!command) return message.channel.send(`Cannot load command \`${args[0]}\``);
+			if(command.name === args[args.length - 1]) return await message.channel.send(`Already have command \`${command.qualifiedName}\` loaded (did you mean to use \`reload\` instead?)`);
+			// TODO: Internally call load command (this command) to attempt to load missing command first
+			if(command.name !== args[args.length - 2]) return await message.channel.send(`Cannot load command \`${args.slice(0, command.qualifiedName.split(" ").length + 1).join(" ")}\``);
+			cmdFile = args[args.length - 1];
+			dirPath = `${process.cwd()}/build/commands/*/${command.qualifiedName.replace(/ /g, "/")}/${cmdFile}.js`;
+		} else {
+			cmdFile = cmd;
+			dirPath = `${process.cwd()}/build/commands/*/${cmdFile}.js`
+		}
+		return await globAsync(dirPath).then((commands: any) => {
+			mainLoop:
+			for (const commandFile of commands) {
+				console.log(commandFile);
+				if(args.length > 1) {
+					let currCommand: Command | SubCommand = command!;
+					const cmdChain: string[] = new RegExp(`${process.cwd().replace(/\\/g, "/")}/build/commands/(.+)/${cmdFile}\.js`).exec(commandFile)![1].split("/").reverse();
+					for(const parent of cmdChain.slice(0, -1)) {
+						if(currCommand instanceof SubCommand) {
+							if(currCommand.name !== parent) continue mainLoop;
+							currCommand = currCommand.parent;
+						} else {
+							if(currCommand.name !== parent) continue mainLoop;
+						}
+					}
+					if(currCommand.category !== cmdChain[cmdChain.length - 1]) continue mainLoop;
+					// Validated path with command parent chain
+
+					delete require.cache[require.resolve(commandFile)];
+					let { name } = path.parse(commandFile);
+					let File = require(commandFile);
+					if (!this.isClass(File.default)) return message.channel.send(`Command ${name} is not an instance of Command`);
+
+					const loadedCommand = new File.default(this.client, command);
+					// any SubCommand is-a Command
+					if (!(loadedCommand instanceof SubCommand)) return message.channel.send(`Event ${name} doesn't belong in commands!`);
+					command!.subCommands.push(loadedCommand);
+					command = loadedCommand;
+				} else {
+					delete require.cache[require.resolve(commandFile)];
+					let { name } = path.parse(commandFile);
+					let File = require(commandFile);
+					if (!this.isClass(File.default)) return message.channel.send(`Command ${name} is not an instance of Command`);
+
+					const loadedCommand = new File.default(this.client, name);
+					if (!(loadedCommand instanceof Command)) return message.channel.send(`Event ${name} doesn't belong in commands!`);
+
+					this.client.commands.set(loadedCommand.name, loadedCommand);
+					if (loadedCommand.aliases.length) {
+						for (const alias of loadedCommand.aliases) {
+							this.client.aliases.set(alias, loadedCommand.name);
+						}
+					}
+					command = loadedCommand;
+				}
+				return message.channel.send(`Loaded command \`${command.qualifiedName}\``);
+			}
+			return message.channel.send(`Cannot load command \`${args.join(" ")}\``);
+		});
+	}
+}
---
+++ src/commands/Admin/command/reload.ts
@@ -0,0 +1,60 @@
+import Command from "../../../structures/Command";
+import SubCommand from "../../../structures/SubCommand";
+import { Message } from "discord.js";
+import BulbBotClient from "../../../structures/BulbBotClient";
+import path from "path";
+
+export default class extends SubCommand {
+
+	private isClass(input: any): boolean {
+		return typeof input === "function" && typeof input.prototype === "object" && input.toString().substring(0, 5) === "class";
+	}
+
+	constructor(client: BulbBotClient, parent: Command) {
+		super(client, parent, {
+			name: "reload",
+			aliases: ["r"],
+			minArgs: 1,
+			maxArgs: -1,
+			argList: ["command:Command|SubCommand"],
+			usage: "<command>",
+		});
+	}
+
+	public async run(message: Message, args: string[]): Promise<void | Message> {
+		let command: Command | undefined = Command.resolve(this.client, args);
+
+		if(!command) return;
+		if(command instanceof SubCommand) {
+			command.parent.subCommands.splice(command.parent.subCommands.findIndex(sc => sc === command), 1);
+		} else {
+			this.client.commands.delete(command.name);
+		}
+		command.aliases.forEach(alias => this.client.aliases.delete(alias));
+
+		// we can do better than this
+		const cmdPath: string = `${command.category}\\${command.qualifiedName.replace(/ /g, "\\")}`;
+		const commandFile: string = `${process.cwd()}/build/commands/${cmdPath}.js`;//.replace(/\//g, "\\");
+		console.log(commandFile);
+		delete require.cache[require.resolve(commandFile)];
+		let { name } = path.parse(commandFile);
+		let File = require(commandFile);
+		if (!this.isClass(File.default)) return message.channel.send(`Command ${name} is not an instance of Command`);
+
+		const loadedCommand = new File.default(this.client, name);
+		// any SubCommand is-a Command
+		if (!(loadedCommand instanceof Command)) return message.channel.send(`Event ${name} doesn't belong in commands!`);
+		if(command instanceof SubCommand) {
+			command.parent.subCommands.push(<SubCommand>loadedCommand);
+		} else {
+			this.client.commands.set(loadedCommand.name, loadedCommand);
+			if (loadedCommand.aliases.length) {
+				for (const alias of loadedCommand.aliases) {
+					this.client.aliases.set(alias, loadedCommand.name);
+				}
+			}
+		}
+		//return message.channel.send(`Cannot load command \`${args.join(" ")}\``)
+		return message.channel.send(`Reloaded command \`${loadedCommand.qualifiedName}\``)
+	}
+}
