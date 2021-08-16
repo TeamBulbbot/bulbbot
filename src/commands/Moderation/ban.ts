1efb715eeb1f0894e24d3c307ab983be0cfdc23b
KlukCZ <L.Neumajer@seznam.cz>
2021-08-16 18:05:44 +0200

    Added force-ban confirmation

--- src/commands/Moderation/ban.ts
+++ src/commands/Moderation/ban.ts
@@ -1,9 +1,10 @@
 import Command from "../../structures/Command";
-import { Collection, Guild, GuildMember, Message, Snowflake, User } from "discord.js";
+import { ButtonInteraction, Collection, Guild, GuildMember, Message, MessageActionRow, MessageButton, Snowflake, User } from "discord.js";
 import { NonDigits } from "../../utils/Regex";
 import InfractionsManager from "../../utils/managers/InfractionsManager";
 import { BanType } from "../../utils/types/BanType";
 import BulbBotClient from "../../structures/BulbBotClient";
+import * as Emotes from "../../emotes.json";
 
 const infractionsManager: InfractionsManager = new InfractionsManager();
 
@@ -25,13 +26,13 @@ export default class extends Command {
 		});
 	}
 
-	async run(message: Message, args: string[]): Promise<void> {
+	async run(message: Message, args: string[]): Promise<void | Message> {
 		//Variable declarations
 		const targetID: Snowflake = args[0].replace(NonDigits, "");
 		let target: any = message.guild?.members.cache.get(targetID);
 		let reason: string = args.slice(1).join(" ");
 		let notInGuild: boolean = !target;
-		let infID: number;
+		let infID: number = 0;
 
 		if (!notInGuild) {
 			if (await this.client.bulbutils.resolveUserHandle(message, this.client.bulbutils.checkUser(message, target), target.user)) return;
@@ -71,20 +72,46 @@ export default class extends Command {
 
 		//If the user is not in the guild force ban
 		if (notInGuild) {
-			infID = await infractionsManager.ban(
-				this.client,
-				<Guild>message.guild,
-				BanType.FORCE,
-				<User>target,
-				<GuildMember>message.member,
-				await this.client.bulbutils.translate("global_mod_action_log", message.guild?.id, {
-					action: await this.client.bulbutils.translate("mod_action_types.force_ban", message.guild?.id, {}),
-					moderator: message.author,
-					target,
+			const row = new MessageActionRow().addComponents([
+				new MessageButton().setStyle("SUCCESS").setEmoji(Emotes.other.SUCCESS).setLabel("Confirm").setCustomId("confirm"),
+				new MessageButton().setStyle("DANGER").setEmoji(Emotes.other.FAIL).setLabel("Cancel").setCustomId("cancel"),
+			]);
+
+			const confirmMsg = await message.channel.send({
+				content: await this.client.bulbutils.translate("ban_force_confirm", message.guild?.id, { target }),
+				components: [row],
+			});
+
+			const filter = (i: ButtonInteraction) => i.user.id === message.author.id;
+			let interaction: ButtonInteraction;
+
+			try {
+				interaction = await confirmMsg.awaitMessageComponent({ filter, time: 15000 });
+			} catch (_) {
+				await confirmMsg.delete();
+				return await message.channel.send(await this.client.bulbutils.translate("global_execution_cancel", message.guild?.id, {}));
+			}
+
+			if (interaction.customId === "confirm") {
+				await confirmMsg.delete();
+				infID = await infractionsManager.ban(
+					this.client,
+					<Guild>message.guild,
+					BanType.FORCE,
+					<User>target,
+					<GuildMember>message.member,
+					await this.client.bulbutils.translate("global_mod_action_log", message.guild?.id, {
+						action: await this.client.bulbutils.translate("mod_action_types.force_ban", message.guild?.id, {}),
+						moderator: message.author,
+						target,
+						reason,
+					}),
 					reason,
-				}),
-				reason,
-			);
+				);
+			} else {
+				await confirmMsg.delete();
+				return await message.channel.send(await this.client.bulbutils.translate("global_execution_cancel", message.guild?.id, {}));
+			}
 		} else {
 			//Else execute a normal ban
 			target = target.user;
