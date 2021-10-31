9f4386af4a56904d9356ea99b27274ea26f45124
KlukCZ <41321902+KlukCZ@users.noreply.github.com>
2021-10-31 13:12:22 +0100

    Added checks for role update and delete

--- src/events/guild/roles/roleDelete.ts
+++ src/events/guild/roles/roleDelete.ts
@@ -1,8 +1,11 @@
 import { Role } from "discord.js";
 import Event from "../../../structures/Event";
 import LoggingManager from "../../../utils/managers/LoggingManager";
+import DatabaseManager from "../../../utils/managers/DatabaseManager";
+import { GuildConfiguration } from "../../../utils/types/GuildConfiguration";
 
 const loggingManager: LoggingManager = new LoggingManager();
+const databaseManager: DatabaseManager = new DatabaseManager();
 
 export default class extends Event {
 	constructor(...args: any[]) {
@@ -11,7 +14,12 @@ export default class extends Event {
 	}
 
 	async run(role: Role) {
-        // TODO: attempt to fetch executor from audit log
+		const config: GuildConfiguration = await databaseManager.getConfig(role.guild.id);
+		if (role.id === config.muteRole) await databaseManager.setMuteRole(role.guild.id, null);
+		else if (role.id === config.autorole) await databaseManager.setAutoRole(role.guild.id, null);
+
+		// TODO: attempt to fetch executor from audit log
+		// Too lazy to do this right now - Kluk
 		await loggingManager.sendEventLog(this.client, role.guild, "role", await this.client.bulbutils.translate("event_role_delete", role.guild.id, { role: role }));
 	}
 }
