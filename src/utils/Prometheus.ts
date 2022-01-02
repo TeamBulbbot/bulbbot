8731a4dbe33433812b33612da6027d073ebef318
y3ll0w <46920058+y3ll0wlife@users.noreply.github.com>
2022-01-02 15:34:53 +0100

    Update Prometheus.ts

--- src/utils/Prometheus.ts
+++ src/utils/Prometheus.ts
@@ -2,36 +2,26 @@ import prom from "prom-client";
 import http from "http";
 import url from "url";
 
-/*
-Guild Data
-	- Total Guilds (include unavaible)
-	- Most active guilds
+const latency = new prom.Gauge({ name: "bulbbot_latency", help: "The bulbbot latency to the Discord Websocket" });
+const cachedUsers = new prom.Gauge({ name: "bulbbot_cached_users", help: "The amount of cached users in the memory" });
+const guildCount = new prom.Gauge({ name: "bulbbot_guild_count", help: "The amount of guilds that Bulbbot serves" });
+const guildMemberCount = new prom.Gauge({ name: "bulbbot_guild_member_count", help: "The amount of users Bulbbot serves" });
 
-User Data
-	- Total Users
-	- Most active users
-
-Command Data
-	 - Command Usage
-	 - Commands / hour
-	 - Commands / min
-	 - Commands / sec
-	 - Total Commands
-
-Event Data
-	- Event Usage (from raw event)
-	- Events / hour
-	- Events / min
-	- Events / sec
-	- Total Events
-
-Computer
-	- Memory
-	- CPU
-
-Request per Sec
-
-*/
+const websocket = new prom.Counter({
+	name: "bulbbot_websocket",
+	help: "Analytics regarding the websocket",
+	labelNames: ["event"],
+});
+const userMessages = new prom.Counter({
+	name: "bulbbot_user_messages",
+	help: "Analytics regarding user messages",
+	labelNames: ["authorId"],
+});
+const guildMessages = new prom.Counter({
+	name: "bulbbot_guild_messages",
+	help: "Analytics regarding guild messages",
+	labelNames: ["guildId"],
+});
 
 export async function startPrometheus(client: any): Promise<void> {
 	client.log.info("[PROMETHEUS] Starting up the prometheus");
@@ -42,24 +32,10 @@ export async function startPrometheus(client: any): Promise<void> {
 
 	prom.collectDefaultMetrics({ register });
 
-	const latency = new prom.Gauge({ name: "bulbbot_latency", help: "The bulbbot latency to the Discord Websocket" });
-	const cachedUsers = new prom.Gauge({ name: "bulbbot_cached_users", help: "The amount of cached users in the memory" });
-
-	const websocket = new prom.Counter({
-		name: "bulbbot_websocket",
-		help: "Analytics regarding the websocket",
-		labelNames: ["event"],
+	const metric = [latency, cachedUsers, websocket, userMessages, guildMessages, guildCount, guildMemberCount];
+	metric.forEach(metric => {
+		register.registerMetric(metric);
 	});
-	const messages = new prom.Counter({
-		name: "bulbbot_messages",
-		help: "Analytics regarding messages",
-		labelNames: ["bulbbot"],
-	});
-
-	register.registerMetric(latency);
-	register.registerMetric(cachedUsers);
-	register.registerMetric(websocket);
-	register.registerMetric(messages);
 
 	const server = http.createServer(async (req, res) => {
 		const route = url.parse(req.url!).pathname;
@@ -68,6 +44,8 @@ export async function startPrometheus(client: any): Promise<void> {
 			res.setHeader("Content-Type", register.contentType);
 			latency.set(client.ws.ping);
 			cachedUsers.set(client.users.cache.size);
+			guildCount.set(client.guilds.cache.size);
+			guildMemberCount.set(client.guilds.cache.reduce((a: any, g: { memberCount: any }) => a + g.memberCount, 0));
 
 			res.write(await register.metrics());
 			res.end();
@@ -75,12 +53,12 @@ export async function startPrometheus(client: any): Promise<void> {
 	});
 
 	client.on("raw", (packet: any) => {
-		console.log(packet);
 		if (packet.t === null) return;
 		websocket.inc({ event: packet.t });
 
 		if (packet.t === "MESSAGE_CREATE") {
-			if (packet.d.author.id === client.user.id) messages.inc();
+			userMessages.inc({ authorId: packet.d.author.id });
+			guildMessages.inc({ guildId: packet.d.guild_id });
 		}
 	});
 
