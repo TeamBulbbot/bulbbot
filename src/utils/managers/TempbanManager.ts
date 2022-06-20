d6b932aa16d339afcbefbd3dc9f124fbc6a75381
Walker Gray <55608093+wakfi@users.noreply.github.com>
2022-06-19 20:12:26 -1000

    Migrate TempbanManager to prisma

--- src/utils/managers/TempbanManager.ts
+++ src/utils/managers/TempbanManager.ts
@@ -1,66 +1,55 @@
-import { sequelize } from "../database/connection";
 import { Snowflake, GuildMember } from "discord.js";
-import { QueryTypes } from "sequelize";
-import moment from "moment";
+import prisma from "../../prisma";
 
 export default class {
-	public async createTempBan(target: GuildMember, reason: string, expireTime: number, guildId: Snowflake): Promise<void> {
-		await sequelize.query(
-			'INSERT INTO tempbans ("targetTag", "targetId", "gId", reason, "expireTime", "createdAt", "updatedAt", "guildId") VALUES ($TargetTag, $TargetId, $GID, $Reason, $ExpireTime, $CreatedAt, $UpdatedAt, (SELECT id FROM guilds WHERE "guildId" = $GuildId))',
-			{
-				bind: {
-					Reason: reason,
-					TargetTag: target.user.tag,
-					TargetId: target.user.id,
-					ExpireTime: expireTime,
-					GID: guildId,
-					CreatedAt: moment().format(),
-					UpdatedAt: moment().format(),
-					GuildId: guildId,
+	public async createTempBan(target: GuildMember, reason: string, expireTime: number, guildId: Snowflake) {
+		return await prisma.tempban.create({
+			data: {
+				targetId: target.user.id,
+				targetTag: target.user.tag,
+				reason,
+				expireTime,
+				guildId,
+				bulbGuild: {
+					connect: {
+						guildId,
+					},
 				},
-				type: QueryTypes.INSERT,
 			},
-		);
-	}
-
-	public async getTempBan(id: number): Promise<any> {
-		const response: Record<string, any> = await sequelize.query('SELECT * FROM tempbans WHERE "id" = $Id', {
-			bind: { Id: id },
-			type: QueryTypes.SELECT,
 		});
-
-		return response[0];
 	}
 
-	public async getLatestTempBan(target: GuildMember, guildId: Snowflake): Promise<any> {
-		const response: Record<string, any> = await sequelize.query(
-			'SELECT * FROM tempbans WHERE "targetId" = $TargetId AND "guildId" = (SELECT id FROM guilds WHERE "guildId" = $GuildId) ORDER BY id DESC LIMIT 1',
-			{
-				bind: {
-					TargetId: target.user.id,
-					GuildId: guildId,
+	public async getTempBan(id: number) {
+		return await prisma.tempban.findUnique({
+			where: {
+				id,
+			},
+		});
+	}
+
+	public async getLatestTempBan(target: GuildMember, guildId: Snowflake) {
+		return await prisma.tempban.findFirst({
+			where: {
+				targetId: target.user.id,
+				bulbGuild: {
+					guildId,
 				},
-				type: QueryTypes.SELECT,
 			},
-		);
-
-		return response[0];
-	}
-
-	public async deleteTempBan(id: number): Promise<void> {
-		await sequelize.query('DELETE FROM tempbans WHERE "id" = $Id', {
-			bind: {
-				Id: id,
+			orderBy: {
+				createdAt: "desc",
 			},
-			type: QueryTypes.DELETE,
 		});
 	}
 
-	public async getAllTemBans(): Promise<any> {
-		const mutes: Record<string, any> = await sequelize.query("SELECT * FROM tempbans", {
-			type: QueryTypes.SELECT,
+	public async deleteTempBan(id: number) {
+		return await prisma.tempban.delete({
+			where: {
+				id,
+			},
 		});
+	}
 
-		return mutes;
+	public async getAllTemBans() {
+		return await prisma.tempban.findMany();
 	}
 }
