7752c7d2f1aa8674618eac64f7efceb3a32ec132
y3ll0w <46920058+y3ll0wlife@users.noreply.github.com>
2022-06-22 14:13:39 +0200

    moved id to slash commands

--- src/commands/Miscellaneous/id.ts
+++ src/commands/Miscellaneous/id.ts
@@ -1,27 +1,37 @@
-import Command from "../../structures/Command";
-import CommandContext from "../../structures/CommandContext";
-import { AnyId } from "../../utils/Regex";
+import { CommandInteraction } from "discord.js";
 import BulbBotClient from "../../structures/BulbBotClient";
+import ApplicationCommand from "../../structures/ApplicationCommand";
+import { ApplicationCommandType } from "../../utils/types/ApplicationCommands";
+import { ApplicationCommandOptionTypes } from "discord.js/typings/enums";
+import { AnyId } from "../../utils/Regex";
 
-export default class extends Command {
+export default class extends ApplicationCommand {
 	constructor(client: BulbBotClient, name: string) {
 		super(client, {
 			name,
 			description: "Parse out the Discord id from any text",
-			category: "Miscellaneous",
-			usage: "<text>",
-			examples: ["id asdasdasdasdasdas123456789012345678asdasdasd"],
-			argList: ["text:String"],
-			minArgs: 1,
-			maxArgs: -1,
+			type: ApplicationCommandType.CHAT_INPUT,
+			options: [
+				{
+					name: "text",
+					type: ApplicationCommandOptionTypes.STRING,
+					description: "The text you want to parse the ids from",
+					required: true,
+				},
+			],
 		});
 	}
 
-	async run(context: CommandContext, args: string[]) {
-		const ids = args.join(" ").match(AnyId);
+	public async run(interaction: CommandInteraction) {
+		const ids = interaction.options.getString("text", true).match(AnyId);
 
-		if (!ids) return context.channel.send(await this.client.bulbutils.translate("id_no_found", context.guild?.id, {}));
-		context.channel.send(ids.join("\n"));
+		if (!ids)
+			return interaction.reply({
+				content: await this.client.bulbutils.translate("id_no_found", interaction.guild?.id, {}),
+				ephemeral: true,
+			});
+
+		interaction.reply(ids.join("\n"));
 
 		return;
 	}
