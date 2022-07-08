5f48f61b2a5fd220d41aa0d880b7fc82e5c7ad2c
KlukCZ <41321902+KlukCZ@users.noreply.github.com>
2022-07-08 14:13:18 +0200

    Migrated purge user

--- src/commands/Moderation/purge.ts
+++ src/commands/Moderation/purge.ts
@@ -2,6 +2,7 @@ import BulbBotClient from "../../structures/BulbBotClient";
 import ApplicationCommand from "../../structures/ApplicationCommand";
 import { ApplicationCommandType } from "discord-api-types/v10";
 import all from "./purge/all";
+import user from "./purge/user";
 
 export default class extends ApplicationCommand {
 	constructor(client: BulbBotClient, name: string) {
@@ -10,7 +11,7 @@ export default class extends ApplicationCommand {
 			description: "Purge messages from a channel.",
 			type: ApplicationCommandType.ChatInput,
 			options: [],
-			subCommands: [all],
+			subCommands: [all, user],
 			command_permissions: ["MANAGE_MESSAGES"],
 			client_permissions: ["MANAGE_MESSAGES"],
 		});
