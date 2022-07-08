ec30539b082608c614d5a64d3ca6475457aa7d31
KlukCZ <41321902+KlukCZ@users.noreply.github.com>
2022-07-08 14:36:54 +0200

    Migrated purge contains

--- src/commands/Moderation/purge.ts
+++ src/commands/Moderation/purge.ts
@@ -4,6 +4,7 @@ import { ApplicationCommandType } from "discord-api-types/v10";
 import all from "./purge/all";
 import user from "./purge/user";
 import bots from "./purge/bots";
+import contains from "./purge/contains";
 
 export default class extends ApplicationCommand {
 	constructor(client: BulbBotClient, name: string) {
@@ -12,7 +13,7 @@ export default class extends ApplicationCommand {
 			description: "Purge messages from a channel.",
 			type: ApplicationCommandType.ChatInput,
 			options: [],
-			subCommands: [all, user, bots],
+			subCommands: [all, user, bots, contains],
 			command_permissions: ["MANAGE_MESSAGES"],
 			client_permissions: ["MANAGE_MESSAGES"],
 		});
--- src/commands/Moderation/purge/contains.ts
+++ src/commands/Moderation/purge/contains.ts
@@ -1,33 +1,41 @@
-import Command from "../../../structures/Command";
-import SubCommand from "../../../structures/SubCommand";
-import CommandContext from "../../../structures/CommandContext";
-import { Collection, Message, Snowflake } from "discord.js";
+import { Collection, CommandInteraction, Guild, GuildTextBasedChannel, Message, Snowflake } from "discord.js";
 import moment from "moment";
 import { writeFileSync } from "fs";
 import LoggingManager from "../../../utils/managers/LoggingManager";
 import BulbBotClient from "../../../structures/BulbBotClient";
-import { isGuildChannel } from "../../../utils/typechecks";
+import ApplicationSubCommand from "../../../structures/ApplicationSubCommand";
+import ApplicationCommand from "../../../structures/ApplicationCommand";
+import { ApplicationCommandOptionType } from "discord-api-types/v10";
 
 const loggingManager: LoggingManager = new LoggingManager();
 
-export default class extends SubCommand {
-	constructor(client: BulbBotClient, parent: Command) {
+export default class extends ApplicationSubCommand {
+	constructor(client: BulbBotClient, parent: ApplicationCommand) {
 		super(client, parent, {
 			name: "contains",
-			clearance: 50,
-			minArgs: 2,
-			maxArgs: 2,
-			argList: ["query:String", "amount:Number"],
-			usage: "<query> <amount>",
 			description: "Purges messages containing the query.",
+			options: [
+				{
+					name: "query",
+					description: "The query to search for.",
+					type: ApplicationCommandOptionType.String,
+					required: true,
+				},
+				{
+					name: "amount",
+					description: "The amount of messages to purge.",
+					type: ApplicationCommandOptionType.Integer,
+					required: true,
+					min_value: 2,
+					max_value: 499,
+				},
+			],
 		});
 	}
 
-	public async run(context: CommandContext, args: string[]): Promise<void | Message> {
-		let amount = Number(args[1]);
-		if (amount >= 500) return context.channel.send(await this.client.bulbutils.translate("purge_too_many", context.guild?.id, {}));
-		if (amount < 2 || isNaN(amount)) return context.channel.send(await this.client.bulbutils.translate("purge_too_few", context.guild?.id, {}));
-		if (!isGuildChannel(context.channel)) return;
+	public async run(interaction: CommandInteraction): Promise<void> {
+		const query = interaction.options.getString("query") as string;
+		let amount = interaction.options.getInteger("amount") as number;
 
 		const deleteMsg: number[] = [];
 		let a = 0;
@@ -40,7 +48,11 @@ export default class extends SubCommand {
 		}
 		if (amount - a !== 0) deleteMsg.push(amount - a);
 
-		let delMsgs = `Message purge in #${context.channel.name} (${context.channel.id}) by ${context.author.tag} (${context.author.id}) at ${moment().format("MMMM Do YYYY, h:mm:ss a")} \n`;
+		let delMsgs = await this.client.bulbutils.translate("purge_message_log", interaction.guild?.id, {
+			user: interaction.user,
+			channel: interaction.channel as GuildTextBasedChannel,
+			timestamp: moment().format("MMMM Do YYYY, h:mm:ss a"),
+		});
 
 		const messagesToPurge: Snowflake[] = [];
 		amount = 0;
@@ -48,12 +60,12 @@ export default class extends SubCommand {
 		const twoWeeksAgo = moment().subtract(14, "days").unix();
 
 		for (let i = 0; i < deleteMsg.length; i++) {
-			const msgs: Collection<string, Message> = await context.channel.messages.fetch({
+			const msgs: Collection<string, Message> = await (interaction.channel as GuildTextBasedChannel)?.messages.fetch({
 				limit: deleteMsg[i],
 			});
 
-			const regex = new RegExp(`(?:^|\\W)${args[0]}(?:$|\\W)`, "gi");
-			msgs.map(async (m) => {
+			const regex = new RegExp(`(?:^|\\W)${query}(?:$|\\W)`, "gi");
+			msgs.map(async (m: Message) => {
 				if (moment(m.createdAt).unix() < twoWeeksAgo) msgs.delete(m.id);
 				if (m.content.match(regex)) {
 					delMsgs += `${moment(m.createdTimestamp).format("MM/DD/YYYY, h:mm:ss a")} | ${m.author.tag} (${m.author.id}) | ${m.id} | ${m.content} |\n`;
@@ -63,12 +75,20 @@ export default class extends SubCommand {
 			});
 		}
 
-		await context.channel.bulkDelete(messagesToPurge);
+		await (interaction.channel as GuildTextBasedChannel)?.bulkDelete(messagesToPurge);
 
-		writeFileSync(`${__dirname}/../../../../files/PURGE-${context.guild?.id}.txt`, delMsgs);
+		writeFileSync(`${__dirname}/../../../../files/PURGE-${interaction.guild?.id}.txt`, delMsgs);
 
-		await loggingManager.sendModActionFile(this.client, context.guild, "purge", amount, `${__dirname}/../../../../files/PURGE-${context.guild?.id}.txt`, context.channel, context.author);
+		await loggingManager.sendModActionFile(
+			this.client,
+			interaction.guild as Guild,
+			"purge",
+			amount,
+			`${__dirname}/../../../../files/PURGE-${interaction.guild?.id}.txt`,
+			interaction.channel as GuildTextBasedChannel,
+			interaction.user,
+		);
 
-		await context.channel.send(await this.client.bulbutils.translate("purge_success", context.guild?.id, { count: amount }));
+		return interaction.reply(await this.client.bulbutils.translate("purge_success", interaction.guild?.id, { count: amount }));
 	}
 }
