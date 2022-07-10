c27ec73babd8f84c370725d1d7b3ed1e4a6c374b
KlukCZ <41321902+KlukCZ@users.noreply.github.com>
2022-07-10 19:03:31 +0200

    Forgot to remove this

--- src/commands/Information/charinfo.ts
+++ src/commands/Information/charinfo.ts
@@ -1,6 +1,4 @@
-import Command from "../../structures/Command";
-import CommandContext from "../../structures/CommandContext";
-import { CommandInteraction, Message } from "discord.js";
+import { CommandInteraction } from "discord.js";
 import BulbBotClient from "../../structures/BulbBotClient";
 import ApplicationCommand from "../../structures/ApplicationCommand";
 import { ApplicationCommandOptionType, ApplicationCommandType } from "discord-api-types/v9";
@@ -70,70 +68,5 @@ export default class extends ApplicationCommand {
 		else if (text.length >= 1000) text = text.substring(0, 1000) + "...";
 
 		return interaction.reply(`\`\`\`${text}\`\`\``);
-	}
-}
-
-export class lol extends Command {
-	constructor(client: BulbBotClient, name: string) {
-		super(client, {
-			name,
-			description: "Returns information about the characters provided",
-			category: "Information",
-			usage: "<characters>",
-			examples: ["charinfo 🍰"],
-			argList: ["characters:String"],
-			minArgs: 1,
-			maxArgs: -1,
-		});
-	}
-
-	public async run(context: CommandContext, args: string[]): Promise<void | Message> {
-		const categories: string[] = [
-			"Cc",
-			"Zs",
-			"Po",
-			"Sc",
-			"Ps",
-			"Pe",
-			"Sm",
-			"Pd",
-			"Nd",
-			"Lu",
-			"Sk",
-			"Pc",
-			"Ll",
-			"So",
-			"Lo",
-			"Pi",
-			"Cf",
-			"No",
-			"Pf",
-			"Lt",
-			"Lm",
-			"Mn",
-			"Me",
-			"Mc",
-			"Nl",
-			"Zl",
-			"Zp",
-			"Cs",
-			"Co",
-		];
-
-		const chars: string = args.join(" ");
-		let text = "";
-
-		for (let i = 0; i < chars.length; i++) {
-			categories.forEach((cat) => {
-				const Unicode = require(`unicode/category/${cat}`);
-				const charCode = Unicode[chars[i].charCodeAt(0)];
-				if (charCode !== undefined) return (text += `${chars[i]} - ${charCode.value} | ${charCode.name}\n`);
-				return "";
-			});
-		}
-		if (text === "") text = "Unable to find any data about the given character(s)";
-		else if (text.length >= 1000) text = text.substring(0, 1000) + "...";
-
-		return context.channel.send(`\`\`\`${text}\`\`\``);
 	}
 }
