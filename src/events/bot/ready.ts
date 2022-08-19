0fc08c30b9694a0f8d708c4c07275b377e24de7f
Philip <46920058+y3ll0wlife@users.noreply.github.com>
2022-08-19 13:29:35 +0200

    Update ready.ts

--- src/events/bot/ready.ts
+++ src/events/bot/ready.ts
@@ -24,8 +24,6 @@ export default class extends Event {
 
 		await loadReminders(this.client);
 		await loadTempBans(this.client);
-		
-		console.log("hello world");
 
 		registerSlashCommands(this.client);
 
